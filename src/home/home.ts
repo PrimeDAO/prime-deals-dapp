e28ef18182d6ee96a5f701d14e32f0a5ea83f578
Gilad Tsabar <2517870+giladt@users.noreply.github.com>
2021-08-12 08:12:13 +0200

    update to latest homepage design

--- src/home/home.ts
+++ src/home/home.ts
@@ -9,8 +9,10 @@ import "./home.scss";
 @autoinject
 export class Home {
 
-  featuredSeeds: Array<any> = null;
-  runningSeeds: Array<any> = null;
+  public featuredDeals: Array<any> = null;
+  public cardIndex: number;
+  private _openDeals: Array<any> = null;
+  private _runningDeals: Array<any> = null;
 
   constructor(
     private router: Router,
@@ -24,11 +26,14 @@ export class Home {
     // this.seedService.getFeaturedSeeds().then((seeds) => {
     //   this.featuredSeeds = seeds;
     // });
-    this.featuredSeeds = [
+    this._openDeals = [
       {
         address: "0x1jk3lk4353l45kj345l3k45j345",
-        name: "PrimeDAO",
-        logo: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+        dao: "PrimeDAO",
+        description: "Lorem ipsum dolor sit amet, consectetur adi piscing elit. Ut pretium pretium tempor. Uteget imperdiet neque. In volutpat ante semper diam molestie.",
+        logo: {
+          creator: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+        },
         canGoToDashboard: true,
         contributingIsOpen: false,
         uninitialized: false,
@@ -41,8 +46,11 @@ export class Home {
       },
       {
         address: "0x1jk3lk4353l45kj345l3k45j345",
-        name: "Uniswap",
-        logo: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/uniswap.png",
+        dao: "Uniswap",
+        description: "Lorem ipsum dolor sit amet, consectetur adi piscing elit. Ut pretium pretium tempor. Uteget imperdiet neque. In volutpat ante semper diam molestie.",
+        logo: {
+          creator: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/uniswap.png",
+        },
         canGoToDashboard: false,
         contributingIsOpen: false,
         uninitialized: false,
@@ -55,8 +63,11 @@ export class Home {
       },
       {
         address: "0x1jk3lk4353l45kj345l3k45j345",
-        name: "Compound",
-        logo: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/compound.png",
+        dao: "Compound",
+        description: "Lorem ipsum dolor sit amet, consectetur adi piscing elit. Ut pretium pretium tempor. Uteget imperdiet neque. In volutpat ante semper diam molestie.",
+        logo: {
+          creator: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/compound.png",
+        },
         canGoToDashboard: true,
         contributingIsOpen: false,
         uninitialized: false,
@@ -69,8 +80,11 @@ export class Home {
       },
       {
         address: "0x1jk3lk4353l45kj345l3k45j345",
-        name: "Fei",
-        logo: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/fei.png",
+        dao: "Fei",
+        description: "Lorem ipsum dolor sit amet, consectetur adi piscing elit. Ut pretium pretium tempor. Uteget imperdiet neque. In volutpat ante semper diam molestie.",
+        logo: {
+          creator: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/fei.png",
+        },
         canGoToDashboard: true,
         contributingIsOpen: false,
         uninitialized: false,
@@ -83,8 +97,11 @@ export class Home {
       },
       {
         address: "0x1jk3lk4353l45kj345l3k45j345",
-        name: "Open DAO 5",
-        logo: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+        dao: "Open DAO 5",
+        description: "Lorem ipsum dolor sit amet, consectetur adi piscing elit. Ut pretium pretium tempor. Uteget imperdiet neque. In volutpat ante semper diam molestie.",
+        logo: {
+          creator: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+        },
         canGoToDashboard: true,
         contributingIsOpen: false,
         uninitialized: false,
@@ -97,8 +114,11 @@ export class Home {
       },
       {
         address: "0x1jk3lk4353l45kj345l3k45j345",
-        name: "Open DAO 6",
-        logo: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+        dao: "Open DAO 6",
+        description: "Lorem ipsum dolor sit amet, consectetur adi piscing elit. Ut pretium pretium tempor. Uteget imperdiet neque. In volutpat ante semper diam molestie.",
+        logo: {
+          creator: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+        },
         canGoToDashboard: true,
         contributingIsOpen: false,
         uninitialized: false,
@@ -111,8 +131,11 @@ export class Home {
       },
       {
         address: "0x1jk3lk4353l45kj345l3k45j345",
-        name: "Open DAO 7",
-        logo: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+        dao: "Open DAO 7",
+        description: "Lorem ipsum dolor sit amet, consectetur adi piscing elit. Ut pretium pretium tempor. Uteget imperdiet neque. In volutpat ante semper diam molestie.",
+        logo: {
+          creator: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+        },
         canGoToDashboard: true,
         contributingIsOpen: false,
         uninitialized: false,
@@ -125,8 +148,11 @@ export class Home {
       },
       {
         address: "0x1jk3lk4353l45kj345l3k45j345",
-        name: "Open DAO 8",
-        logo: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+        dao: "Open DAO 8",
+        description: "Lorem ipsum dolor sit amet, consectetur adi piscing elit. Ut pretium pretium tempor. Uteget imperdiet neque. In volutpat ante semper diam molestie.",
+        logo: {
+          creator: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+        },
         canGoToDashboard: true,
         contributingIsOpen: false,
         uninitialized: false,
@@ -139,8 +165,11 @@ export class Home {
       },
       {
         address: "0x1jk3lk4353l45kj345l3k45j345",
-        name: "Open DAO 9",
-        logo: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+        dao: "Open DAO 9",
+        description: "Lorem ipsum dolor sit amet, consectetur adi piscing elit. Ut pretium pretium tempor. Uteget imperdiet neque. In volutpat ante semper diam molestie.",
+        logo: {
+          creator: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+        },
         canGoToDashboard: false,
         contributingIsOpen: false,
         uninitialized: false,
@@ -153,8 +182,11 @@ export class Home {
       },
       {
         address: "0x1jk3lk4353l45kj345l3k45j345",
-        name: "Open DAO 10",
-        logo: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+        dao: "Open DAO 10",
+        description: "Lorem ipsum dolor sit amet, consectetur adi piscing elit. Ut pretium pretium tempor. Uteget imperdiet neque. In volutpat ante semper diam molestie.",
+        logo: {
+          creator: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+        },
         canGoToDashboard: true,
         contributingIsOpen: false,
         uninitialized: false,
@@ -167,68 +199,194 @@ export class Home {
       },
     ];
 
-    this.runningSeeds = [
+    this._runningDeals = [
       {
         address: "0x1jk3lk4353l45kj345l3k45j345",
-        name: "Running DAO 1",
-        logo: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+        dao: {
+          creator: "DAO Creator 1",
+          partner: "DAO Partner 1",
+        },
+        proposal: {
+          title: "Proposal Title",
+          description: "Lorem ipsum dolor sit amet, consectetur adi piscing elit. Ut pretium pretium tempor. Uteget imperdiet neque. In volutpat ante semper diam molestie.",
+        },
+        logo: {
+          creator: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+          partner: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/uniswap.png",
+        },
+        canGoToDashboard: true,
+        description: "Lorem ipsum dolor sit amet, consectetur adi piscing elit. Ut pretium pretium tempor. Uteget imperdiet neque. In volutpat ante semper diam molestie.",
+        contributingIsOpen: false,
+        uninitialized: false,
+        hasNotStarted: true,
+        hideIcons: false,
+        isPaused: false,
+        isClosed: false,
+        claimingIsOpen: false,
+        startsInMilliseconds: new Date().getUTCMilliseconds() + 0.15*1000*60*60*24,
+      },
+      {
+        address: "0x1jk3lk4353l45kj345l3k45j345",
+        dao: {
+          creator: "DAO Creator 2",
+          partner: "DAO Partner 2",
+        },
+        proposal: {
+          title: "Proposal Title",
+          description: "Lorem ipsum dolor sit amet, consectetur adi piscing elit. Ut pretium pretium tempor. Uteget imperdiet neque. In volutpat ante semper diam molestie.",
+        },
+        logo: {
+          creator: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+          partner: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/compound.png",
+        },
         canGoToDashboard: true,
       },
       {
         address: "0x1jk3lk4353l45kj345l3k45j345",
-        name: "Running DAO 2",
-        logo: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+        dao: {
+          creator: "DAO Creator 3",
+          partner: "DAO Partner 3",
+        },
+        proposal: {
+          title: "Proposal Title",
+          description: "Lorem ipsum dolor sit amet, consectetur adi piscing elit. Ut pretium pretium tempor. Uteget imperdiet neque. In volutpat ante semper diam molestie.",
+        },
+        logo: {
+          creator: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+          partner: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/fei.png",
+        },
         canGoToDashboard: true,
       },
       {
         address: "0x1jk3lk4353l45kj345l3k45j345",
-        name: "Running DAO 3",
-        logo: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+        dao: {
+          creator: "DAO Creator 4",
+          partner: "DAO Partner 4",
+        },
+        proposal: {
+          title: "Proposal Title",
+          description: "Lorem ipsum dolor sit amet, consectetur adi piscing elit. Ut pretium pretium tempor. Uteget imperdiet neque. In volutpat ante semper diam molestie.",
+        },
+        logo: {
+          creator: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+          partner: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+        },
         canGoToDashboard: true,
       },
       {
         address: "0x1jk3lk4353l45kj345l3k45j345",
-        name: "Running DAO 4",
-        logo: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+        dao: {
+          creator: "DAO Creator 5",
+          partner: "DAO Partner 5",
+        },
+        proposal: {
+          title: "Proposal Title",
+          description: "Lorem ipsum dolor sit amet, consectetur adi piscing elit. Ut pretium pretium tempor. Uteget imperdiet neque. In volutpat ante semper diam molestie.",
+        },
+        logo: {
+          creator: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+          partner: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+        },
         canGoToDashboard: true,
       },
       {
         address: "0x1jk3lk4353l45kj345l3k45j345",
-        name: "Running DAO 5",
-        logo: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+        dao: {
+          creator: "DAO Creator 6",
+          partner: "DAO Partner 6",
+        },
+        proposal: {
+          title: "Proposal Title",
+          description: "Lorem ipsum dolor sit amet, consectetur adi piscing elit. Ut pretium pretium tempor. Uteget imperdiet neque. In volutpat ante semper diam molestie.",
+        },
+        logo: {
+          creator: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+          partner: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+        },
         canGoToDashboard: true,
       },
       {
         address: "0x1jk3lk4353l45kj345l3k45j345",
-        name: "Running DAO 6",
-        logo: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+        dao: {
+          creator: "DAO Creator 7",
+          partner: "DAO Partner 7",
+        },
+        proposal: {
+          title: "Proposal Title",
+          description: "Lorem ipsum dolor sit amet, consectetur adi piscing elit. Ut pretium pretium tempor. Uteget imperdiet neque. In volutpat ante semper diam molestie.",
+        },
+        logo: {
+          creator: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+          partner: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+        },
         canGoToDashboard: true,
       },
       {
         address: "0x1jk3lk4353l45kj345l3k45j345",
-        name: "Running DAO 7",
-        logo: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+        dao: {
+          creator: "DAO Creator 8",
+          partner: "DAO Partner 8",
+        },
+        proposal: {
+          title: "Proposal Title",
+          description: "Lorem ipsum dolor sit amet, consectetur adi piscing elit. Ut pretium pretium tempor. Uteget imperdiet neque. In volutpat ante semper diam molestie.",
+        },
+        logo: {
+          creator: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+          partner: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+        },
         canGoToDashboard: true,
       },
       {
         address: "0x1jk3lk4353l45kj345l3k45j345",
-        name: "Running DAO 8",
-        logo: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+        dao: {
+          creator: "DAO Creator 9",
+          partner: "DAO Partner 9",
+        },
+        proposal: {
+          title: "Proposal Title",
+          description: "Lorem ipsum dolor sit amet, consectetur adi piscing elit. Ut pretium pretium tempor. Uteget imperdiet neque. In volutpat ante semper diam molestie.",
+        },
+        logo: {
+          creator: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+          partner: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+        },
         canGoToDashboard: true,
       },
       {
         address: "0x1jk3lk4353l45kj345l3k45j345",
-        name: "Running DAO 9",
-        logo: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
-        canGoToDashboard: true,
-      },
-      {
-        address: "0x1jk3lk4353l45kj345l3k45j345",
-        name: "Running DAO 10",
-        logo: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+        dao: {
+          creator: "DAO Creator 10",
+          partner: "DAO Partner  10",
+        },
+        proposal: {
+          title: "Proposal Title",
+          description: "Lorem ipsum dolor sit amet, consectetur adi piscing elit. Ut pretium pretium tempor. Uteget imperdiet neque. In volutpat ante semper diam molestie.",
+        },
+        logo: {
+          creator: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+          partner: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+        },
         canGoToDashboard: true,
       },
     ];
+
+    this.featuredDeals = [...this._openDeals];
+    this.cardIndex = 0;
+  }
+
+  dealToggle(index = 0):void {
+    switch (index) {
+      case 0:
+        this.featuredDeals = [...this._openDeals];
+        break;
+      case 1:
+        this.featuredDeals = [...this._runningDeals];
+        break;
+      default:
+        this.featuredDeals = [...this._openDeals];
+    }
+    this.cardIndex = index;
   }
 
   navigate(href: string): void {
