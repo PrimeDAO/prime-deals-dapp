f3016816227518f6924cd8c86d961004faf7e351
Gilad Tsabar <2517870+giladt@users.noreply.github.com>
2021-08-16 15:41:50 +0200

    small refactor and cleanup

--- src/registry-wizard/stage2.ts
+++ src/registry-wizard/stage2.ts
@@ -1,6 +1,6 @@
 import { BaseStage } from "registry-wizard/baseStage";
 import { Utils } from "services/utils";
-
+import { IDAO } from "./seedConfig";
 export class Stage2 extends BaseStage {
 
   // Add a link object to the link object arrays
@@ -26,8 +26,6 @@ export class Stage2 extends BaseStage {
 
   addToken(daoIndex: number): void {
     // Create a new token object
-    console.log({daoIndex});
-
     this.seedConfig.daos[daoIndex].tokens.push({name: undefined, amount: undefined});
   }
   // Delete a row in the tokens array
@@ -38,28 +36,35 @@ export class Stage2 extends BaseStage {
 
   validateInputs(): Promise<string> {
     let message: string;
-    if (!this.seedConfig.daos[0].name) {
-      message = `Please enter a name DAO ${0 + 1}`;
-    }
-    // Validate the tokens
-    this.seedConfig.daos[0].tokens.forEach((token: {name:string, amount:number}) => {
-      if (!token.name) {
-        // Current input as not been filled out
-        message = "Please enter a name for the token";
-      } else if (!token.amount) {
-        // Current input as not been filled out
-        message = `Please enter funding amount for the token ${token.name}`;
+    this.seedConfig.daos.forEach((dao:IDAO, idx:number) => {
+      if (!dao.name) {
+        message = `Please enter a name for DAO ${(idx + 1)}`;
       }
-    });
 
-    // Validate the links
-    this.seedConfig.daos[0].social_medias.forEach((platform: {name:string, url:string}) => {
-      if (!platform.name) {
-        // Current input as not been filled out
-        message = "Please enter a name of a platform";
-      } else if (!Utils.isValidUrl(platform.url, false)) {
-        // Current input as not been filled out
-        message = `Please enter a valid URL for ${platform.name}`;
+      if (!message) {
+        // Validate the tokens
+        dao.tokens.forEach((token: {name:string, amount:number}) => {
+          if (!token.name) {
+            // Current input as not been filled out
+            message = `Missing a token name by  ${dao.name || "DAO " + (idx + 1)}`;
+          } else if (!token.amount) {
+            // Current input as not been filled out
+            message = `Missing funding amount for the token ${token.name} by ${dao.name || "DAO " + (idx + 1)}`;
+          }
+        });
+      }
+
+      if (!message) {
+        // Validate the links
+        dao.social_medias.forEach((platform: {name:string, url:string}) => {
+          if (!platform.name) {
+            // Current input as not been filled out
+            message = `Missing a social media name by ${dao.name || "DAO " + (idx + 1)}`;
+          } else if (!Utils.isValidUrl(platform.url, false)) {
+            // Current input as not been filled out
+            message = `Invalid URL for the social media ${platform.name} by ${dao.name || "DAO " + (idx + 1)}`;
+          }
+        });
       }
     });
 
