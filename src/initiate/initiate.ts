61ce87b6cfbadc179f6f62e8c645a01f39f60275
Gilad Tsabar <2517870+giladt@users.noreply.github.com>
2021-08-28 14:20:32 +0200

    refactor initiate page to have new buttons style

--- src/initiate/initiate.ts
+++ src/initiate/initiate.ts
@@ -1,23 +1,34 @@
+import { Router } from "aurelia-router";
+import { autoinject } from "aurelia-framework";
 import "./initiate.scss";
 
 interface IBoxes {
-  name: string
-  slug: string
-  isDisabled: boolean
+  name: string,
+  slug: string,
+  isDisabled: boolean,
+  slot: string,
 }
 
+@autoinject
 export class Initiate {
-  box: IBoxes[] = [
+  boxes: IBoxes[] = [
     {
       name: "Joint Venture",
       slug: "joint-venture",
       isDisabled: true,
+      slot: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat.",
     },
     {
       name: "Token Swap",
       slug: "token-swap",
       isDisabled: false,
+      slot: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat.",
     },
   ];
-}
 
+  constructor(private router: Router) { }
+
+  navigate(href: string, args): void {
+    this.router.navigateToRoute(href, args);
+  }
+}
