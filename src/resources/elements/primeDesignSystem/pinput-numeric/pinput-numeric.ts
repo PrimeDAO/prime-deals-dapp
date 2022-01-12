bf791b075c78707988c6e372041b2255656dcaf5
Ionel lupu <ionel.cristian.lupu@gmail.com>
2022-01-12 18:18:59 +0200

    pinput-numeric component (#65)
    * created the pinput-numeric component
    
    * properly styling the input based on the design system
    
    * properly styling the input based on the design system
    
    * properly styling the input based on the design system
    
    * extracted redundant styles from the pinput-numeric component
    
    * updated the pinput-numeric based in numeric-input changes
    
    * fixed conflicts and moved ValidationState enum in a types.ts file

---
+++ src/resources/elements/primeDesignSystem/pinput-numeric/pinput-numeric.ts
@@ -0,0 +1,26 @@
+import {bindable} from "aurelia-typed-observable-plugin";
+import {bindingMode, customElement} from "aurelia-framework";
+import "./pinput-numeric.scss";
+import {BigNumber} from "ethers";
+import {ValidationState} from "../types";
+
+@customElement("pinput-numeric")
+export class PInputNumeric {
+
+  @bindable public validationState = ValidationState.ok;
+
+  /**
+   * Look into the numeric-input for more info about the below properties
+   */
+  @bindable.booleanAttr public decimal = true;
+  @bindable.string public defaultText = "";
+  @bindable public handleChange: ({keyCode: number}) => boolean;
+  @bindable public autocomplete = "off";
+  @bindable.booleanAttr public disabled;
+  @bindable({ defaultBindingMode: bindingMode.twoWay }) public value: number | BigNumber | string;
+  @bindable.booleanAttr public notWei?: boolean = false;
+  @bindable.number public decimals?: number = 18;
+  @bindable.booleanAttr public outputAsString?: boolean = false;
+  @bindable.string public placeholder = "";
+  @bindable inFocus = false; //  attribute name "focus" doesn't work
+}
