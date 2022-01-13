a5d9e217ae11dd82ee1b7678ea00fd71831e7fa1
Ionel Lupu <ionel.cristian.lupu@gmail.com>
2022-01-13 22:09:04 +0200

    improved inputs in the design system

--- src/resources/elements/primeDesignSystem/pinput-text/pinput-text.ts
+++ src/resources/elements/primeDesignSystem/pinput-text/pinput-text.ts
@@ -6,11 +6,9 @@ import {ValidationState} from "../types";
 @customElement("pinput-text")
 export class PInputText {
 
-  @bindable public validationState = ValidationState.ok;
-
+  @bindable public validationState?: ValidationState;
   @bindable public autocomplete = "off";
   @bindable.booleanAttr public disabled;
   @bindable({defaultBindingMode: bindingMode.twoWay}) public value: string;
   @bindable.string public placeholder = "";
-  @bindable inFocus = false; //  attribute name "focus" doesn't work
 }
