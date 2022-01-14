100d3cc7c96b69e23348b42c6cf4d7ddf81119cd
Gilad <gilad@tsabar.net>
2022-01-14 20:14:13 +0100

    added deal dashboard draft styling with discussion

--- src/dealDashboard/dealDashboard.ts
+++ src/dealDashboard/dealDashboard.ts
@@ -1,13 +1,16 @@
-// import { Router } from "aurelia-router";
 import { autoinject } from "aurelia-framework";
 import { EthereumService } from "services/EthereumService";
+import { EventAggregator, Subscription } from "aurelia-event-aggregator";
 import { DateService } from "services/DateService";
+import { Router, RouterConfiguration } from "aurelia-router";
+import { PLATFORM } from "aurelia-pal";
 import "./dealDashboard.scss";
+import { DiscussionsService } from "./discussionsService";
 
 export interface IClause {
-  id: string,
-  text: string,
+  description: string,
   discussionThread: {
+    threadId: string,
     creator: string,
     createdAt: Date,
   }
@@ -15,51 +18,52 @@ export interface IClause {
 
 @autoinject
 export class DealDashboard {
-  loading = true;
+  // loading = true;
   connected = false;
+  private router: Router;
+  private routeChangeEvent: Subscription;
+  private activeClause = "";
 
   // TODO: get from a service
   dealClauses: Array<IClause> = [
     {
-      id: "1",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna",
       discussionThread: {
-        creator: "John Doe",
-        createdAt: new Date(2022, 0, 1),
-      },
-      // replies: 2,
-      // lastActivity: Math.floor(this.dateService.getDurationBetween(new Date(), new Date(2022, 0, 2)).asDays()),
-    },
-    {
-      id: "2",
-      text: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Ut enim ad minim veniam, quis nostr",
-      discussionThread: {
+        threadId: "0x891ac160551a35b586b7e304aac9287904773bd7047388111ceefac5d3a108bc",
         creator: "John Doe",
         createdAt: new Date(2022, 0, 1),
       },
     },
     {
-      id: "3",
-      text: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      description: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Ut enim ad minim veniam, quis nostr",
       discussionThread: {
+        threadId: "0xae506662f1b14ab626da1df812f43ecffb9fbae37c43a43b42263b3d89494958",
+        creator: "John Doe",
+        createdAt: new Date(2022, 0, 1),
+      },
+    },
+    {
+      description: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      discussionThread: {
+        threadId: "0x54d1c476fe9997e6ad758cc8ca32160f5deef3fbc4e57f747f433978af8a0c11",
         creator: "John Doe",
         createdAt: new Date(2022, 0, 2),
       },
     },
     {
-      id: "4",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna",
+      description: "Culpa qui officia deserunt mollit anim id est laborum.",
       discussionThread: {
+        threadId: undefined,
         creator: "John Doe",
-        createdAt: new Date(2022, 0, 1),
+        createdAt: new Date(2022, 0, 2),
       },
     },
     {
-      id: "5",
-      text: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      description: "Excepteur sint occaecat cupidatat id est laborum.",
       discussionThread: {
+        threadId: undefined,
         creator: "John Doe",
-        createdAt: new Date(2022, 0, 1),
+        createdAt: new Date(2022, 0, 2),
       },
     },
   ];
@@ -67,7 +71,62 @@ export class DealDashboard {
   constructor(
     private ethereumService: EthereumService,
     private dateService: DateService,
+    private discussionsService: DiscussionsService,
+    private eventAggregator: EventAggregator,
   ) {
     this.connected = !!this.ethereumService.defaultAccountAddress;
+  }
+
+  activate(_, __, navigationInstruction) {
+    this.setThreadIdFromRoute(navigationInstruction);
+
+    this.routeChangeEvent = this.eventAggregator.subscribe("router:navigation:complete", (response) => {
+      this.setThreadIdFromRoute(response.instruction);
+    });
+  }
+
+  deactivate() {
+    this.routeChangeEvent.dispose();
+  }
+
+  private setThreadIdFromRoute(navigationInstruction): void {
+    const currentRoute = navigationInstruction.params.childRoute;
+    if (currentRoute && currentRoute.includes("/")) {
+      this.activeClause = navigationInstruction.params.childRoute.split("/")[1];
+    } else {
+      this.activeClause = "";
+    }
+  }
+  private addDiscussion = async (topic: string, id: string): Promise<boolean> => {
+    this.activeClause = await this.discussionsService.createDiscussion(topic);
+    this.dealClauses[id].discussionThread.threadId = this.activeClause;
+
+    this.router.navigate(`thread/${this.activeClause}` );
+    return true;
+  };
+
+  private configureRouter(config: RouterConfiguration, router: Router): void {
+    const routes = [
+      {
+        route: [""],
+        nav: true,
+        moduleId: PLATFORM.moduleName("./discussionsList/discussionsList"),
+        name: "discussions-list",
+        title: "Discussions",
+      },
+      {
+        route: ["thread"],
+        href: "thread/:threadId",
+        nav: true,
+        moduleId: PLATFORM.moduleName("./dealThread/dealThread"),
+        name: "deal-thread",
+        title: "Thread",
+      },
+
+    ];
+
+    config.map(routes);
+
+    this.router = router;
   }
 }
---
+++ src/dealDashboard/discussionsService.ts
@@ -0,0 +1,105 @@
+import { EthereumService } from "services/EthereumService";
+import { autoinject, bindable } from "aurelia-framework";
+import { IDiscussion } from "./discussionsList/discussionsList";
+import { ethers } from "ethers";
+import { ConsoleLogService } from "services/ConsoleLogService";
+// import { Realtime } from "ably/promises";
+
+export interface IComment {
+  _id: string,
+  text: string,
+  author: string,
+  metadata: any,
+  replyTo?: string,
+  upvotes: Array<string>,
+  downvotes: Array<string>,
+  timestamp: number,
+}
+
+export interface IProfile {
+  address: string,
+  image: string,
+  name: string,
+}
+
+@autoinject
+export class DiscussionsService {
+
+  private comments: Array<IComment> = [];
+  private showReply = "";
+  private channelName: string;
+  private channel: any;
+
+  @bindable private comment: string;
+  @bindable private inputRef: HTMLTextAreaElement;
+  @bindable private dealId: string;
+
+  constructor(
+    // private realtime: Realtime,
+    private ethereumService: EthereumService,
+    private consoleLogService: ConsoleLogService,
+  ) {
+    this.init();
+  }
+
+  @bindable public discussions = null;
+  // TODO: discuss service
+
+  public init(): void {
+    // auth init
+    // this.channelName = `deal-${this.dealId}`; /* Hash it? */
+    // const ably = new Realtime.Promise({authUrl: `https://theconvo.space/api/getAblyAuth?apikey=${ process.env.CONVO_API_KEY}}`});
+
+    // this.channel = await ably.channels.get(this.channelName);
+
+    // const signer = this.ethereumService.getDefaultSigner();
+    this.getDiscussions();
+  }
+
+  private getDiscussions = async (): Promise<void> => {
+    try {
+      if (localStorage.length) {
+        this.discussions = JSON.parse(localStorage.getItem("discussions") || "{}");
+        if (Object.keys(this.discussions).length === 0) {
+          localStorage.setItem("discussions", JSON.stringify(this.discussions));
+        }
+      }
+    } catch (error) {
+      this.consoleLogService.logMessage(error);
+    }
+  };
+
+  public async createDiscussion(topic: string): Promise<string> {
+    this.getDiscussions();
+
+    const discussionId = ethers.utils.hashMessage(topic);
+    const newDiscussion: IDiscussion = {
+      topic: topic,
+      creator: this.ethereumService.defaultAccountAddress,
+      createdAt: new Date(),
+      replies: 0,
+      lastActivity: null,
+    };
+
+    this.discussions[discussionId] = newDiscussion;
+    localStorage.setItem("discussions", JSON.stringify(this.discussions));
+    return discussionId;
+  }
+
+  // What is the current user address?
+  // What is the current provider?
+
+  // authenticate wallet (before creating a comment)
+
+  // get user profile
+
+  // get threads list by deal id
+
+  // get all comments by thread id
+
+  // send comment to thread
+
+  // delete comment by id?
+
+  // create new thread for deal id
+}
