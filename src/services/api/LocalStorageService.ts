53926e62f108232ef8728a0614f86fc100f3165d
hiaux0 <hieudn5293@yahoo.de>
2022-01-20 14:08:57 +0100

    refac(service.local): add generics

--- src/services/api/LocalStorageService.ts
+++ src/services/api/LocalStorageService.ts
@@ -5,17 +5,17 @@ const CLAUSES_DATA = "DEALS_STORAGE_V1";
 
 export class LocalStorageService {
   public getClausesData(): IClause[] {
-    const clausesData = this.getItem(CLAUSES_DATA);
-    return clausesData as unknown as IClause[];
+    const clausesData = this.getItem<IClause[]>(CLAUSES_DATA);
+    return clausesData;
   }
 
-  private getItem(key = DEALS_DATA): Record<string, unknown> {
+  private getItem<T>(key = DEALS_DATA): T {
     const result = localStorage.getItem(key);
 
     if (!result) return;
     if (Object.keys(result).length === 0) return;
 
-    return JSON.parse(result);
+    return JSON.parse(result) as T;
   }
 
   setItem(data: Record<string, unknown>): void {
