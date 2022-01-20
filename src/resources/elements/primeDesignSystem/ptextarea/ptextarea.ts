22ca76d8c0b24be176881c9758c0946eb183dc00
Ionel Lupu <ionel.cristian.lupu@gmail.com>
2022-01-20 11:45:23 +0200

    added resize option to textareas

--- src/resources/elements/primeDesignSystem/ptextarea/ptextarea.ts
+++ src/resources/elements/primeDesignSystem/ptextarea/ptextarea.ts
@@ -7,11 +7,12 @@ import {ValidationState} from "../types";
 @customElement("ptextarea")
 export class PTextarea {
 
-  @bindable public validationState?: ValidationState;
+  @bindable validationState?: ValidationState;
 
-  @bindable public autocomplete = "off";
-  @bindable.booleanAttr public disabled;
-  @bindable.number public rows = 4;
-  @bindable({defaultBindingMode: bindingMode.twoWay}) public value: string | BigNumber;
-  @bindable.string public placeholder = "";
+  @bindable autocomplete = "off";
+  @bindable.booleanAttr disabled;
+  @bindable.number rows = 4;
+  @bindable({defaultBindingMode: bindingMode.twoWay}) value: string | BigNumber;
+  @bindable.string placeholder = "";
+  @bindable.booleanAttr resizable = true;
 }
