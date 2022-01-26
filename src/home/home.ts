b6be2e2c571fbab4d432d355a6f4a91ff8e8563b
Ionel lupu <ionel.cristian.lupu@gmail.com>
2022-01-26 23:31:46 +0200

    Updated home page based on the mockup design (#89)
    * updated home page based on the mockup
    
    * used scss variables for "time-left" "pill" components
    
    * added design system variables
    
    * added design system variables
    
    * improved typography

--- src/home/home.ts
+++ src/home/home.ts
@@ -1,33 +1,110 @@
-import { DealService } from "services/DealService";
-import { DateService } from "./../services/DateService";
-import { autoinject, singleton } from "aurelia-framework";
-import { Router } from "aurelia-router";
+import {DealService} from "services/DealService";
+import {autoinject, singleton} from "aurelia-framework";
+import {Router} from "aurelia-router";
 import "./home.scss";
+import {IDummyDeal} from "../entities/IDummyDeal";
 
 @singleton(false)
 @autoinject
 export class Home {
+  cardIndex = 0;
 
-  public featuredDeals: Array<any> = null;
-  public cardIndex: number;
-  private _openDeals: Array<any> = null;
-  private _runningDeals: Array<any> = null;
+  featuredDeals: Array<any> = [];
+  allDeals: Record<"open" | "partnered", Array<IDummyDeal>> = {
+    open: [],
+    partnered: [],
+  };
 
   constructor(
     private router: Router,
     private dealService: DealService,
-    private dateService: DateService,
   ) {
   }
 
   attached(): void {
-    this.dealService.getFeaturedDeals().then((deals) => {
-      this.featuredDeals = [...deals];
-      this._openDeals = [...this.featuredDeals];
-    });
+    // TODO: use dealService when available instead of dummy data.
+    // this.dealService.getFeaturedDeals().then((deals) => {
+    //   this.featuredDeals = [...deals];
+    //   this.openDeals = [...this.featuredDeals];
+    // });
 
-    // this._runningDeals = [...this.featuredDeals];
-    this._runningDeals = [
+    this.allDeals.open = [
+      {
+        address: "0x1jk3lk4353l45kj345l3k45j345",
+        dao: {
+          creator: "DAO Creator 2 With a Looooong Name",
+        },
+        type: "Token Swap",
+        title: "Swap tokenized carbon credits for a better world",
+        description: "Lorem ipsum dolor sit amet, consectetur adi piscing elit. Ut pretium pretium tempor. Uteget imperdiet neque. In volutpat ante semper diam molestie.",
+        logo: {
+          creator: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+        },
+        status: "contributingIsOpen",
+        startsInMilliseconds: new Date().getUTCMilliseconds() + 0.15 * 1000 * 60 * 60 * 24,
+      },
+      {
+        address: "0x1jk3lk4353l45kj345l3k45j345",
+        dao: {
+          creator: "DAO Creator 1",
+        },
+        type: "Joint Venture",
+        title: "Let’s spice up the world together",
+        description: "Lorem ipsum dolor sit amet, consectetur adi piscing elit. Ut pretium pretium tempor. Uteget imperdiet neque. In volutpat ante semper diam molestie.",
+        logo: {
+          creator: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+        },
+        status: "hasNotStarted",
+        startsInMilliseconds: new Date().getUTCMilliseconds() + 0.15 * 1000 * 60 * 60 * 24,
+      },
+      {
+        address: "0x1jk3lk4353l45kj345l3k45j345",
+        dao: {
+          creator: "DAO Creator 2 With a Looooong Name",
+        },
+        type: "Token Swap",
+        title: "Proposal Title",
+        description: "Lorem ipsum dolor sit amet, consectetur adi piscing elit. Ut pretium pretium tempor. Uteget imperdiet neque. In volutpat ante semper diam molestie.",
+        logo: {
+          creator: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+        },
+        status: "claimingIsOpen",
+        startsInMilliseconds: new Date().getUTCMilliseconds() + 0.15 * 1000 * 60 * 60 * 24,
+      },
+    ];
+    this.allDeals.partnered = [
+      {
+        address: "0x1jk3lk4353l45kj345l3k45j345",
+        dao: {
+          creator: "DAO Creator 1",
+          partner: "DAO Partner 1",
+        },
+        type: "Joint Venture",
+        title: "Lorem ipsum dolor sit amet, consectetur adipiscing",
+        description: "Lorem ipsum dolor sit amet, consectetur adi piscing elit. Ut pretium pretium tempor. Uteget imperdiet neque. In volutpat ante semper diam molestie.",
+        logo: {
+          creator: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+          partner: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/uniswap.png",
+        },
+        status: "incomplete",
+        startsInMilliseconds: new Date().getUTCMilliseconds() + 0.15 * 1000 * 60 * 60 * 24,
+      },
+      {
+        address: "0x1jk3lk4353l45kj345l3k45j345",
+        dao: {
+          creator: "DAO Creator 2 With a Looooong Name",
+          partner: "DAO Partner 2",
+        },
+        type: "Token Swap",
+        title: "Proposal Title",
+        description: "Lorem ipsum dolor sit amet, consectetur adi piscing elit. Ut pretium pretium tempor. Uteget imperdiet neque. In volutpat ante semper diam molestie.",
+        logo: {
+          creator: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
+          partner: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/compound.png",
+        },
+        status: "uninitialized",
+        startsInMilliseconds: new Date().getUTCMilliseconds() + 0.15 * 1000 * 60 * 60 * 24,
+      },
       {
         address: "0x1jk3lk4353l45kj345l3k45j345",
         dao: {
@@ -41,41 +118,18 @@ export class Home {
           creator: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
           partner: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/uniswap.png",
         },
-        startsInMilliseconds: new Date().getUTCMilliseconds() + 0.15*1000*60*60*24,
+        status: "isClosed",
+        startsInMilliseconds: new Date().getUTCMilliseconds() + 0.15 * 1000 * 60 * 60 * 24,
       },
-      {
-        address: "0x1jk3lk4353l45kj345l3k45j345",
-        dao: {
-          creator: "DAO Creator 2 With a Looooong Name",
-          partner: "DAO Partner 2",
-        },
-        type: "Joint Venture",
-        title: "Proposal Title",
-        description: "Lorem ipsum dolor sit amet, consectetur adi piscing elit. Ut pretium pretium tempor. Uteget imperdiet neque. In volutpat ante semper diam molestie.",
-        logo: {
-          creator: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/primedao.jpg",
-          partner: "https://deepdao-uploads.s3.us-east-2.amazonaws.com/assets/dao/logo/compound.png",
-        },
-        startsInMilliseconds: new Date().getUTCMilliseconds() + 0.15*1000*60*60*24,
-      },
-
     ];
 
-    // this.featuredDeals = [...this._openDeals];
+    this.featuredDeals = this.allDeals.open;
+
     this.cardIndex = 0;
   }
 
-  dealToggle(index = 0):void {
-    switch (index) {
-      case 0:
-        this.featuredDeals = [...this._openDeals];
-        break;
-      case 1:
-        this.featuredDeals = [...this._runningDeals];
-        break;
-      default:
-        this.featuredDeals = [...this._openDeals];
-    }
+  dealToggle(index: number, type: "open" | "partnered"): void {
+    this.featuredDeals = this.allDeals[type];
     this.cardIndex = index;
   }
 
