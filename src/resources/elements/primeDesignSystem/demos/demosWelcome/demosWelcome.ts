81890aa9abae4764b5d934577c3eed734b793d71
hiaux0 <hieudn5293@yahoo.de>
2022-01-28 13:27:23 +0100

    refac(demos): support view-only components

--- src/resources/elements/primeDesignSystem/demos/demosWelcome/demosWelcome.ts
+++ src/resources/elements/primeDesignSystem/demos/demosWelcome/demosWelcome.ts
@@ -1,14 +1,34 @@
 export class DemosWelcome {
+  view: string;
   viewModel: string;
   componentName: any;
+
+  private onlyViewsList = [
+    "pFormInputDemo",
+    "pInputNumericDemo",
+    "pInputTextDemo",
+    "pTextareaDemo",
+    "pToggleDemo",
+  ]
 
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   activate(params): void {
     if (params.componentName) {
       const { componentName } = params;
       this.componentName = componentName;
-      this.viewModel = `../${componentName}/${componentName}`;
+
+      if (this.onlyViewsList.includes(componentName)) {
+        this.view = getViewPath(componentName);
+        this.viewModel = undefined;
+      } else {
+        this.viewModel = `../${componentName}/${componentName}`;
+        this.view = undefined;
+      }
     }
   }
 
+}
+
+function getViewPath(componentName: string): string {
+  return `../${componentName}/${componentName}.html`;
 }
