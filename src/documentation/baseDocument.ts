6a8f3ec28bf59ac96e114b6c629c03e2141ae272
Doug Kent <github@douglaskent.com>
2022-02-01 19:43:36 -0600

    documentation refactor 2

--- src/documentation/baseDocument.ts
+++ src/documentation/baseDocument.ts
@@ -1,21 +1,12 @@
 import { singleton } from "aurelia-framework";
 import { RouteConfig } from "aurelia-router";
-import {activationStrategy, RoutableComponentDetermineActivationStrategy} from "aurelia-router";
 
 @singleton(true)
-export class BaseDocument implements RoutableComponentDetermineActivationStrategy {
+export class BaseDocument {
   content: string;
   title: string;
   activate(_params: unknown, routeConfig: RouteConfig): void {
     this.title = routeConfig.title;
     this.content = routeConfig.settings.content;
-  }
-
-  /**
-   * so this viewModel will be reactivated on each change
-   * in route (see https://aurelia.io/docs/routing/configuration#reusing-an-existing-view-model)
-   */
-  determineActivationStrategy() {
-    return activationStrategy.replace;
   }
 }
