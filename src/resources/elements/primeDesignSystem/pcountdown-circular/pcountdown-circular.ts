abbd74853a7d243168c1daa190bc754850dfd0d6
Doug Kent <github@douglaskent.com>
2022-02-07 18:37:05 -0600

    pie border working

--- src/resources/elements/primeDesignSystem/pcountdown-circular/pcountdown-circular.ts
+++ src/resources/elements/primeDesignSystem/pcountdown-circular/pcountdown-circular.ts
@@ -31,13 +31,22 @@ export class PCountdownCircular {
   /**
    * radius in pixels.
    */
-  @bindable.number radius = 24;
+  @bindable.number radius = 12;
 
   timerId: any;
   secondsLeft = 0;
   startTime: number;
   pausedTime: number;
   pausedDuration = 0;
+  // canvas: HTMLCanvasElement;
+  isAttached = false;
+
+  attached() {
+    this.isAttached = true;
+    if (this.running) {
+      this.runningChanged(this.running);
+    }
+  }
 
   pausedChanged(nowPaused: boolean) {
     if (nowPaused) {
@@ -49,41 +58,50 @@ export class PCountdownCircular {
 
     Utils.waitUntilTrue(() => this.startAt !== undefined);
 
-    if (running) {
-      this.secondsLeft = this.startAt;
-      this.startTime = Date.now();
+    if (this.isAttached) {
+      if (running) {
+        this.secondsLeft = this.startAt;
+        this.startTime = Date.now();
 
-      if (!this.timerId) {
-        this.paused = false;
+        // const ctx = this.canvas.getContext("2d");
+        // ctx.strokeStyle = "white"; // "#64557d";
+        // ctx.beginPath();
+        // ctx.lineWidth = 2;
+        // ctx.arc(this.radius, this.radius, this.radius, 0, 2*Math.PI);
+        // ctx.stroke();
 
-        this.timerId = setInterval(() => {
-          const currentTime = Date.now();
-          if (this.paused) {
-            this.pausedDuration += (currentTime - this.pausedTime);
-            this.pausedTime = currentTime;
-          } else {
-            const secondsPassed = Math.floor((currentTime - this.startTime - this.pausedDuration) / 1000);
-            const secondsLeft = this.startAt - secondsPassed;
-            let stop = false;
-            if (secondsLeft !== this.secondsLeft) {
-              if (secondsLeft > 0) {
-                this.secondsLeft = secondsLeft;
-              } else { // <= zero
-                stop = true;
-              }
-              if (this.ticked) {
-                setTimeout(() => this.ticked( { secondsLeft: this.secondsLeft }), 100 );
-              }
-              if (stop) {
-                this.stop(false);
+        if (!this.timerId) {
+          this.paused = false;
+
+          this.timerId = setInterval(() => {
+            const currentTime = Date.now();
+            if (this.paused) {
+              this.pausedDuration += (currentTime - this.pausedTime);
+              this.pausedTime = currentTime;
+            } else {
+              const secondsPassed = Math.floor((currentTime - this.startTime - this.pausedDuration) / 1000);
+              const secondsLeft = this.startAt - secondsPassed;
+              let stop = false;
+              if (secondsLeft !== this.secondsLeft) {
+                if (secondsLeft > 0) {
+                  this.secondsLeft = secondsLeft;
+                } else { // <= zero
+                  stop = true;
+                }
+                if (this.ticked) {
+                  setTimeout(() => this.ticked( { secondsLeft: this.secondsLeft }), 100 );
+                }
+                if (stop) {
+                  this.stop(false);
+                }
               }
             }
-          }
-        }, 1000);
-      }
-    } else { // !start
-      if (this.timerId) {
-        this.stop(true);
+          }, 1000);
+        }
+      } else { // !start
+        if (this.timerId) {
+          this.stop(true);
+        }
       }
     }
   }
@@ -97,4 +115,32 @@ export class PCountdownCircular {
       setTimeout(() => this.stopped({ cancelled }), 100);
     }
   }
+
+  // private polarToCartesian(
+  //   centerX: number,
+  //   centerY: number,
+  //   radius: number,
+  //   angleInDegrees: number): { x: number, y: number} {
+  //   const angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
+
+  //   return {
+  //     x: centerX + (radius * Math.cos(angleInRadians)),
+  //     y: centerY + (radius * Math.sin(angleInRadians)),
+  //   };
+  // }
+
+  // private describeArc(x, y, radius, startAngle, endAngle): Array<string> {
+
+  //   const start = this.polarToCartesian(x, y, radius, endAngle);
+  //   const end = this.polarToCartesian(x, y, radius, startAngle);
+
+  //   const largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
+
+  //   const d = [
+  //     "M", start.x, start.y,
+  //     "A", radius, radius, 0, largeArcFlag, 0, end.x, end.y,
+  //   ].join(" ");
+
+  //   return d;
+  // }
 }
