cdd39657a33c4f38d0516901da1ace86bc9a6392
Pawel Janicki <paweljanicki256@gmail.com>
2022-02-09 20:16:09 +0800

    Add DAO stage e2e for create open proposal

--- cypress/integration/tests/deals/create-open-proposal-wizard.e2e.ts
+++ cypress/integration/tests/deals/create-open-proposal-wizard.e2e.ts
@@ -4,6 +4,10 @@ Given("I navigate create open proposal wizard", () => {
   cy.visit("/initiate/token-swap/open-proposal/proposal");
 });
 
+Given("I navigate create open proposal wizard Primary DAO stage", () => {
+  cy.visit("/initiate/token-swap/open-proposal/primary-dao");
+});
+
 Then("I am presented with Open Proposal proposal stage", () => {
   cy.url().should("match", /(initiate\/token-swap\/open-proposal\/proposal$)/)
 })
@@ -30,5 +34,54 @@ Then("I can see stages correct for open proposal", () => {
       cy.contains("div.value", "3").should('be.visible')
       cy.contains("[data-test='pstepper-step-name']", "Primary DAO").should('be.visible')
     })
+  })
+})
+
+Then('I can see Primary DAO section with inputs for collecting its details', () => {
+  cy.get('[data-test="dao-details-section"]').within(() => {
+    cy.get('[data-test="section-title"]').should('have.text', 'Primary DAO')
+    cy.get('[data-test="section-description"]').should('have.text', 'Please fill in the details of the first DAO who will be initiating the deal.')
+
+    cy.contains('pform-input', 'Primary DAO Name').within(() => {
+      cy.get('input').should('exist')
+    })
+    
+    cy.contains('pform-input', 'Primary DAO Treasury Address').within(() => {
+      cy.get('input').should('exist')
+    })
+
+    cy.get('[data-test="dao-avatar-section"]').within(() => {
+      cy.contains('pform-input', 'Primary DAO Avatar').within(() => {
+        cy.get('input').should('exist')
+      })
+
+      cy.get('[data-test="dao-avatar"]')
+        .should('have.css', 'width', '64px')
+        .and('have.css', 'height', '64px')
+    })
+
+    cy.get('[data-test="dao-social-media"]').within(() => {
+      cy.contains('div', 'Social media (optional)');
+      cy.get('[data-test="add-social-media"]').should('have.text', '+ Add social media')
+      cy.get('pselect').within(() => {
+        cy.contains('.ss-option', 'Twitter')
+        cy.contains('.ss-option', 'Discord')
+        cy.contains('.ss-option', 'Telegram')
+        cy.contains('.ss-option', 'Reddit')
+        cy.contains('.ss-option', 'LinkedIn')
+      })
+    })
+  })
+})
+
+Then('I can see Primary DAO Representatives section', () => {
+  cy.get('[data-test="dao-representatives-section"]').within(() => {
+    cy.get('[data-test="section-title"]').should('have.text', 'Representatives')
+    cy.get('[data-test="section-description"]').should('be.visible')
+    cy.contains('div', 'Primary DAO - Representatives Addresses (Max. 5)');
+    cy.get('[data-test="dao-representative"]').should(($item) => {
+      expect($item).to.have.length(1);
+    });
+    cy.get('[data-test="add-dao-representative"]').should('have.text', '+ Additional representative')
   })
 })
---
+++ cypress/integration/tests/deals/shared/dao-stage.ts
@@ -0,0 +1,128 @@
+import { Then, When } from "@badeball/cypress-cucumber-preprocessor/methods";
+
+Then('I am presented with errors for DAO details fields', () => {
+  cy.get('[data-test="dao-name-field"]').within(() => {
+    cy.get('.errorMessage').should('have.text', 'Required Input')
+  })
+  cy.get('[data-test="dao-treasury-field"]').within(() => {
+    cy.get('.errorMessage').should('have.text', 'Required Input')
+  })
+  cy.get('[data-test="dao-avatar-url-field"]').within(() => {
+    cy.get('.errorMessage').should('have.text', 'Required Input')
+  })
+})
+
+Then('I can add link to DAO avatar', () => {
+  cy.get('[data-test="dao-avatar-section"]').within(() => {
+    const avatarUrl = 'https://assets.website-files.com/608bd350d67fe62ab7818c74/6138bc3d43a38d248fae12ce_Prime%20Network%20_%20Simplified.png';
+    cy.get('[data-test="dao-avatar-url-field"]').within(() => {
+      cy.get('input').type(avatarUrl)
+    })
+
+    cy.get('[data-test="dao-avatar"]').should('have.attr', 'src', avatarUrl)
+  })
+})
+
+Then('I can add up to 5 social media', () => {
+  cy.get('[data-test="dao-social-media-list"]').within(() => {
+    cy.get('[data-test="dao-social-media-item"]').should(($socialMedias) => {
+      expect($socialMedias).to.have.length(1);
+    });
+    cy.get('[data-test="add-social-media"]').click();
+    cy.get('[data-test="dao-social-media-item"]').should(($socialMedias) => {
+      expect($socialMedias).to.have.length(2);
+    });
+    cy.get('[data-test="add-social-media"]').click();
+    cy.get('[data-test="dao-social-media-item"]').should(($socialMedias) => {
+      expect($socialMedias).to.have.length(3);
+    });
+    cy.get('[data-test="add-social-media"]').click();
+    cy.get('[data-test="dao-social-media-item"]').should(($socialMedias) => {
+      expect($socialMedias).to.have.length(4);
+    });
+    cy.get('[data-test="add-social-media"]').click();
+    cy.get('[data-test="dao-social-media-item"]').should(($socialMedias) => {
+      expect($socialMedias).to.have.length(5);
+    });
+    cy.get('[data-test="add-social-media"]').should('not.exist')
+  });
+})
+
+Then('I can remove all social media', () => {
+  cy.get('[data-test="dao-social-media-item"]').should(($socialMedias) => {
+    expect($socialMedias).to.have.length(5);
+  });
+  cy.get('[data-test="remove-social-media"]').last().click();
+  cy.get('[data-test="remove-social-media"]').last().click();
+  cy.get('[data-test="remove-social-media"]').last().click();
+  cy.get('[data-test="remove-social-media"]').last().click();
+  cy.get('[data-test="remove-social-media"]').last().click();
+  cy.get('[data-test="remove-social-media"]').should('not.exist')
+})
+
+Then('No errors for DAO details fields are visible', () => {
+  cy.get('[data-test="dao-name-field"]').within(() => {
+    cy.get('.errorMessage').should('not.exist')
+  })
+  cy.get('[data-test="dao-treasury-field"]').within(() => {
+    cy.get('.errorMessage').should('not.exist')
+  })
+  cy.get('[data-test="dao-avatar-url-field"]').within(() => {
+    cy.get('.errorMessage').should('not.exist')
+  })
+})
+
+Then('I can add up to 5 DAO representatives', () => {
+  cy.get('[data-test="dao-representatives-section"]').within(() => {
+    cy.get('[data-test="dao-representative"]').should(($representatives) => {
+      expect($representatives).to.have.length(1);
+    });
+    cy.get('[data-test="add-dao-representative"]').click();
+    cy.get('[data-test="dao-representative"]').should(($representatives) => {
+      expect($representatives).to.have.length(2);
+    });
+    cy.get('[data-test="add-dao-representative"]').click();
+    cy.get('[data-test="dao-representative"]').should(($representatives) => {
+      expect($representatives).to.have.length(3);
+    });
+    cy.get('[data-test="add-dao-representative"]').click();
+    cy.get('[data-test="dao-representative"]').should(($representatives) => {
+      expect($representatives).to.have.length(4);
+    });
+    cy.get('[data-test="add-dao-representative"]').click();
+    cy.get('[data-test="dao-representative"]').should(($representatives) => {
+      expect($representatives).to.have.length(5);
+    });
+    cy.get('[data-test="add-dao-representative"]').should('not.exist')
+  });
+})
+
+Then('I can remove all but one DAO representative', () => {
+  cy.get('[data-test="dao-representative"]').should(($representatives) => {
+    expect($representatives).to.have.length(5);
+  });
+  cy.get('[data-test="remove-dao-representative"]').last().click();
+  cy.get('[data-test="remove-dao-representative"]').last().click();
+  cy.get('[data-test="remove-dao-representative"]').last().click();
+  cy.get('[data-test="remove-dao-representative"]').last().click();
+  cy.get('[data-test="remove-dao-representative"]').should('not.exist')
+})
+
+Then('I am presented with error about missing representative address', () => {
+  cy.get('[data-test="dao-representative"] pform-input').within(() => {
+    cy.get('.errorMessage').should('have.text', 'Required Input')
+  })
+})
+
+When('I add 2 incorrect representative addresses', () => {
+  cy.get('[data-test="add-dao-representative"]').click();
+  cy.get('[data-test="dao-representative"] pform-input input').each(($el) => {
+    cy.wrap($el).type('incorrect address')
+  })
+})
+
+Then('I am presented with errors about incorrect representative addresses', () => {
+  cy.get('[data-test="dao-representative"] pform-input .errorMessage').each(($el) => {
+    cy.wrap($el).should('have.text', 'Please enter a valid wallet address')
+  })
+})
