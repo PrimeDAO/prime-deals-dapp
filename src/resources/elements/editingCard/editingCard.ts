df4701204c8180e90ada8201566a4b2f149df7aa
hiaux0 <hieudn5293@yahoo.de>
2022-02-15 21:32:03 +0100

    feat(editingCard): add button logic, that switches view modes

--- src/resources/elements/editingCard/editingCard.ts
+++ src/resources/elements/editingCard/editingCard.ts
@@ -1,5 +1,24 @@
+import { bindable } from "aurelia-framework";
 import "./editingCard.scss";
 
 export class EditingCard {
+  @bindable onDelete: () => void;
+  @bindable onEdit: () => void;
+  @bindable onSave: () => void;
+
   private viewMode: "edit" | "view" = "edit";
+
+  edit() {
+    this.viewMode = "edit";
+    this.onEdit();
+  }
+
+  save() {
+    this.viewMode = "view";
+    this.onSave();
+  }
+
+  delete() {
+    this.onDelete();
+  }
 }
