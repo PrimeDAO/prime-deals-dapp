c75d5431661ba1c970a8ef68113f358ad78ce3bd
hiaux0 <hieudn5293@yahoo.de>
2022-02-24 13:04:08 +0100

    fix(stages): show submit button on last stage

--- src/wizards/tokenSwapDealWizard/components/stageButtons/stageButtons.ts
+++ src/wizards/tokenSwapDealWizard/components/stageButtons/stageButtons.ts
@@ -1,13 +1,17 @@
 import { autoinject } from "aurelia-framework";
 import { bindable } from "aurelia-typed-observable-plugin";
-import { WizardService } from "../../../services/WizardService";
+import { IDealRegistrationTokenSwap } from "entities/DealRegistrationTokenSwap";
+import { IWizardState, WizardService } from "../../../services/WizardService";
 
 import "./stageButtons.scss";
 
 @autoinject
 export class stageButtons {
   @bindable wizardManager: any;
-  @bindable showSubmit = false;
+  @bindable showSubmit;
+
+  private wizardState: IWizardState<IDealRegistrationTokenSwap>;
+
   validating = false;
 
   constructor(public wizardService: WizardService) {
@@ -16,5 +20,21 @@ export class stageButtons {
   async proceed() {
     this.validating = true;
     await this.wizardService.proceed(this.wizardManager).finally(() => this.validating = false);
+  }
+
+  attached() {
+    this.wizardState = this.wizardService.getWizardState(this.wizardManager);
+
+    if (this.showSubmit === undefined) {
+      this.showSubmit = this.showSubmitButton();
+    }
+  }
+
+  showSubmitButton() {
+    const lastStageIndex = this.wizardState.stages.length - 1;
+    const currentStageIndex = this.wizardState.indexOfActive;
+    const isLastStage = lastStageIndex === currentStageIndex;
+
+    return isLastStage;
   }
 }
--- src/wizards/tokenSwapDealWizard/stages/submitStage/submitStage.ts
+++ src/wizards/tokenSwapDealWizard/stages/submitStage/submitStage.ts
@@ -2,11 +2,12 @@ import { autoinject } from "aurelia-framework";
 import { IDealRegistrationTokenSwap } from "entities/DealRegistrationTokenSwap";
 import { IWizardState, WizardService } from "wizards/services/WizardService";
 import { IStageMeta } from "wizards/tokenSwapDealWizard/dealWizardTypes";
+import { WizardManager } from "wizards/tokenSwapDealWizard/wizardManager";
 import "./submitStage.scss";
 
 @autoinject()
 export class SubmitStage {
-  public wizardManager: any;
+  public wizardManager: WizardManager;
   public wizardState: IWizardState<IDealRegistrationTokenSwap>;
   private submitData: IDealRegistrationTokenSwap;
 
