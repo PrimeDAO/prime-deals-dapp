10b6efbd0ea5b6a57798db6e77eef234c83a47fd
Doug Kent <github@douglaskent.com>
2022-02-28 21:30:26 -0600

    upgrade documents

--- src/documentation/documentation.ts
+++ src/documentation/documentation.ts
@@ -1,5 +1,5 @@
 import { PLATFORM } from "aurelia-pal";
-import { singleton } from "aurelia-framework";
+import { singleton, computedFrom } from "aurelia-framework";
 import { Router, RouterConfiguration } from "aurelia-router";
 import {activationStrategy } from "aurelia-router";
 
@@ -8,36 +8,32 @@ import "./documentation.scss";
 @singleton(false)
 export class Documentation {
   router: Router;
+  numDocs: number;
+
+  @computedFrom("router.currentInstruction")
+  get nextDocTitle(): string {
+    const docNumber = this.router.currentInstruction.config.settings.docNumber;
+    if (docNumber < this.numDocs) {
+      return this.router.routes[docNumber + 1].title;
+    } else {
+      return "";
+    }
+  }
+
+  @computedFrom("router.currentInstruction")
+  get previousDocTitle(): string {
+    const docNumber = this.router.currentInstruction.config.settings.docNumber;
+    if (docNumber > 1) {
+      return this.router.routes[docNumber - 1].title;
+    } else {
+      return "";
+    }
+  }
 
   async configureRouter(config: RouterConfiguration, router: Router): Promise<void> {
     config.title = "Documentation";
 
     const documentsSpec = require("./documents.json").documents;
-
-    // const routes = [];
-    // let first = true;
-
-    // for (const docspec of documentsSpec) {
-    //   const filespec = `./${docspec.filespec}`;
-    //   const md = await import(filespec);
-    //   const route = {
-    //     route: [docspec.title.replace(" ", "-")],
-    //     nav: true,
-    //     moduleId: PLATFORM.moduleName("./baseDocument"),
-    //     title: docspec.title,
-    //     activationStrategy: activationStrategy.replace,
-    //     settings: {
-    //       content: md,
-    //     },
-    //   };
-    //   if (first) {
-    //     route.route.push("");
-    //     first = false;
-    //   }
-    //   routes.push(route);
-    // }
-
-    // const docsFilesContext = (require as any).context("/src/documentation/documents/", true, /\.md$/);
 
     /**
      * activationStrategy is docspec.filespecso baseDocument will be reactivated on each change
@@ -45,13 +41,14 @@ export class Documentation {
      */
     const routes = documentsSpec.map((docspec: {title: string, url: string }, ndx: number) => {
       const route = {
-        route: [docspec.title.replace(" ", "-")],
+        route: [docspec.title.replaceAll(" ", "")],
         nav: true,
         moduleId: PLATFORM.moduleName("./baseDocument"),
         title: docspec.title,
         activationStrategy: activationStrategy.replace,
         settings: {
           content: docspec.url,
+          docNumber: ndx+1,
         },
       };
       if (ndx === 0) {
@@ -62,6 +59,24 @@ export class Documentation {
 
     config.map(routes);
 
+    this.numDocs = documentsSpec.length;
+
     this.router = router;
+  }
+
+  next(): void {
+    const docNumber = this.router.currentInstruction.config.settings.docNumber;
+    if (docNumber < 6) {
+      // @ts-ignore
+      this.router.navigate(this.router.routes[docNumber + 1].route);
+    }
+  }
+
+  previous(): void {
+    const docNumber = this.router.currentInstruction.config.settings.docNumber;
+    if (docNumber > 1) {
+      // @ts-ignore
+      this.router.navigate(this.router.routes[docNumber - 1].route);
+    }
   }
 }
