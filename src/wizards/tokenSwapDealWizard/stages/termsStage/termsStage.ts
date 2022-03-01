53a1e29e67c055503fc55a9c46044df44f5db5fa
Ionel Lupu <ionel.cristian.lupu@gmail.com>
2022-03-01 22:12:20 +0200

    refactored and clean form validation code in the registration wizard

--- src/wizards/tokenSwapDealWizard/stages/termsStage/termsStage.ts
+++ src/wizards/tokenSwapDealWizard/stages/termsStage/termsStage.ts
@@ -1,16 +1,10 @@
-import { IStageMeta } from "../../dealWizardTypes";
+import { IBaseWizardStage, IStageMeta } from "../../dealWizardTypes";
 import { autoinject } from "aurelia-framework";
-import { IBaseWizardStage } from "../../dealWizardTypes";
 import { IWizardState, WizardService } from "../../../services/WizardService";
-import {
-  IClause,
-  IDealRegistrationTokenSwap,
-} from "entities/DealRegistrationTokenSwap";
-import {
-  ValidationController,
-  ValidationControllerFactory,
-} from "aurelia-validation";
+import { IClause, IDealRegistrationTokenSwap } from "entities/DealRegistrationTokenSwap";
+import { ValidationController } from "aurelia-validation";
 import "./termsStage.scss";
+import { areFormsValid } from "../../../../services/ValidationService";
 
 @autoinject
 export class TermsStage implements IBaseWizardStage {
@@ -18,18 +12,15 @@ export class TermsStage implements IBaseWizardStage {
   public wizardState: IWizardState<IDealRegistrationTokenSwap>;
 
   private termsForms: ValidationController[] = [];
-  private form: ValidationController;
 
-  constructor(
-    public wizardService: WizardService,
-    private validationControllerFactory: ValidationControllerFactory,
-  ) {}
+  constructor(public wizardService: WizardService) {
+  }
 
   activate(stageMeta: IStageMeta): void {
     this.wizardManager = stageMeta.wizardManager;
     this.wizardState = this.wizardService.getWizardState(this.wizardManager);
 
-    this.addValidation();
+    this.wizardService.registerStageValidateFunction(this.wizardManager, () => areFormsValid(this.termsForms));
   }
 
   onDelete(index: number) {
@@ -42,34 +33,11 @@ export class TermsStage implements IBaseWizardStage {
     this.wizardState.registrationData.terms.clauses.splice(index, 1);
   }
 
-  addClauseButton() {
+  addClause() {
     const emptyClause: IClause = {
       id: "",
       text: "",
     };
     this.wizardState.registrationData.terms.clauses.push(emptyClause);
-  }
-
-  addValidation() {
-    this.form = this.validationControllerFactory.createForCurrentScope();
-
-    this.wizardService.registerStageValidateFunction(
-      this.wizardManager,
-      async () => {
-        const termsValidationResults = await Promise.all(
-          this.termsForms.map((form) =>
-            form.validate().then((result) => result.valid),
-          ),
-        );
-
-        return this.form
-          .validate()
-          .then(
-            (result) =>
-              result.valid &&
-              Boolean(termsValidationResults.filter(Boolean).length),
-          );
-      },
-    );
   }
 }
