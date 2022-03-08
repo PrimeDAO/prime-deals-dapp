9e0867d28e269de1190631778cb9286b0cd2882f
hiaux0 <hieudn5293@yahoo.de>
2022-03-08 10:21:49 +0100

    feat(submit): improve optional indicator

--- src/wizards/tokenSwapDealWizard/stages/submitStage/daoInformation/daoInformation.ts
+++ src/wizards/tokenSwapDealWizard/stages/submitStage/daoInformation/daoInformation.ts
@@ -4,4 +4,17 @@ import "./daoInformation.scss";
 
 export class DaoInformation {
   @bindable daoData: IDAO;
+
+  private isEmptySocialMedia(): boolean {
+    if (this.daoData.social_medias.length === 0) return true;
+
+    if (this.daoData.social_medias.length === 1) {
+      const emptyName = this.daoData.social_medias[0].name === "";
+      const emptyUrl = this.daoData.social_medias[0].url === "";
+      const isEmpyt = emptyName && emptyUrl;
+      return isEmpyt;
+    }
+
+    return false;
+  }
 }
