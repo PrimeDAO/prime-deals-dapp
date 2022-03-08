4103781543aa639c47254fc2e8f6cf074491a417
Ionel Lupu <ionel.cristian.lupu@gmail.com>
2022-03-08 12:36:27 +0200

    Allow appendTo be a string as well on top of what Tippy.js requires it to be

--- src/resources/elements/primeDesignSystem/ptooltip/ptooltip.ts
+++ src/resources/elements/primeDesignSystem/ptooltip/ptooltip.ts
@@ -10,7 +10,7 @@ export class PTooltip {
   @bindable.boolean interactive = false;
   @bindable.boolean visible = true;
   @bindable.boolean allowHtml = false;
-  @bindable appendTo: Element;
+  @bindable appendTo: Props["appendTo"] | string;
 
   private tooltip: Instance;
 
@@ -18,13 +18,18 @@ export class PTooltip {
     this.tooltip = tippy(this.element, {theme: "prime-design-system"});
   }
 
-  propertyChanged(name: string, newValue: string) {
+  propertyChanged(name: string, newValue: any) {
     // Aurelia doesn't trigger change for properties like `allowHTML`. It wants properties to be like this:`allowHtml`.
     // But Tippy.js wants that property to be `allowHTML`, so we need to convert it
     const convertPropertyToTippyConfig: Partial<Record<keyof PTooltip, keyof Props>> = {
       allowHtml: "allowHTML",
     };
     name = convertPropertyToTippyConfig[name] ?? name;
+
+    // Allow appendTo be a string as well on top of what Tippy.js requires it to be
+    if (name === "appendTo" && newValue !== "parent" && typeof newValue === "string") {
+      newValue = document.querySelector(newValue);
+    }
 
     // this works only if this component's properties have the same name as the tippy.js config properties
     // additional properties (like 'visible' for ex.) need to be handled separately (see the 'visibleChanged' method)
