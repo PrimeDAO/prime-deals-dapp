391f35564aafca49a88675d7fc7e95c33c109c0a
Brandon Seydel <brandonseydel@hotmail.com>
2022-03-11 16:30:37 -0600

    fix mock data to be correct for coingecko token service add currency formatter

---
+++ src/resources/value-converters/currency.ts
@@ -0,0 +1,12 @@
+﻿import { autoinject } from "aurelia-framework";
+import { NumberService } from "../../services/NumberService";
+
+@autoinject
+export class CurrencyValueConverter {
+
+  constructor(private numberService: NumberService) { }
+
+  public toView(value: string, decimalPlaces = 0): string | number {
+    return this.numberService.formatCurrency(value, decimalPlaces);
+  }
+}
