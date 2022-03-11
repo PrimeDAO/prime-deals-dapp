1243df11c9053fbffc7dd41c2a32c4462795ee87
Doug Kent <github@douglaskent.com>
2022-03-11 08:02:24 -0600

    get token price in getTokenGeckoInfo

--- src/services/TokenService.ts
+++ src/services/TokenService.ts
@@ -259,7 +259,7 @@ export class TokenService {
 
       return axios.get(uri)
         .then((response) => {
-          // tokenInfo.price = response.data.market_data.current_price.usd ?? 0;
+          tokenInfo.price = response.data.market_data.current_price.usd ?? 0;
           // tokenInfo.priceChangePercentage_24h = response.data.market_data.price_change_percentage_24h ?? 0;
           if (!tokenInfo.logoURI || (tokenInfo.logoURI === TokenService.DefaultLogoURI)) {
             tokenInfo.logoURI = response.data.image.thumb;
