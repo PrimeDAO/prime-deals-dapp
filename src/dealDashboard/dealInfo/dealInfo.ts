24ed3655c17626dae63b0f4060a74c7d93331b6d
Ionel Lupu <ionel.cristian.lupu@gmail.com>
2022-03-15 19:31:23 +0200

    fixed linting issues

--- src/dealDashboard/dealInfo/dealInfo.ts
+++ src/dealDashboard/dealInfo/dealInfo.ts
@@ -2,7 +2,6 @@ import { autoinject, bindable } from "aurelia-framework";
 import { DealTokenSwap } from "entities/DealTokenSwap";
 import "./dealInfo.scss";
 
-
 @autoinject
 export class DealInfo {
   @bindable deal: DealTokenSwap;
--- src/dealDashboard/dealNavbar/dealNavbar.ts
+++ src/dealDashboard/dealNavbar/dealNavbar.ts
@@ -2,7 +2,6 @@ import { autoinject, bindable } from "aurelia-framework";
 import { DealTokenSwap } from "entities/DealTokenSwap";
 import "./dealNavbar.scss";
 
-
 @autoinject
 export class DealNavbar {
   @bindable deal: DealTokenSwap;
