31041a96737dd58bda9494b9ff781b6723a68cb1
Ionel Lupu <ionel.cristian.lupu@gmail.com>
2022-03-15 21:34:50 +0200

    reorganized components

--- src/dealDashboard/dealClauses/dealClauses.ts
+++ src/dealDashboard/dealClauses/dealClauses.ts
@@ -1,10 +1,50 @@
-import { autoinject } from "aurelia-framework";
+import { autoinject, bindingMode } from "aurelia-framework";
 import { bindable } from "aurelia-typed-observable-plugin";
 import { DealTokenSwap } from "entities/DealTokenSwap";
 import "./dealClauses.scss";
+import { EthereumService } from "../../services/EthereumService";
+import { DiscussionsService } from "../discussionsService";
+import { IClause } from "../../entities/DealRegistrationTokenSwap";
 
 @autoinject
 export class DealClauses {
   @bindable deal: DealTokenSwap;
   @bindable.booleanAttr authorized = false;
+  @bindable.string({defaultBindingMode: bindingMode.twoWay}) discussionId?: string;
+
+  private dealId: string;
+  private clauses: IClause[];
+
+  constructor(
+    private ethereumService: EthereumService,
+    private discussionsService: DiscussionsService,
+  ) {
+  }
+
+  attached(): void {
+    this.clauses = this.deal.registrationData.terms.clauses;
+  }
+
+  /**
+   * Adds a new discussion thread to the deal
+   * (Currently saves the thread to the local storage- this should be replaced with a data-storage call)
+   *
+   * @param topic the discussion topic
+   * @param id the id of the clause the discussion is for or null if it is a general discussion
+   */
+  private async addOrReadDiscussion(topic: string, discussionHash: string, clauseHash: string | null, clauseIndex: number | null): Promise<void> {
+    this.discussionId = discussionHash || // If no discussion hash provided- create a new discussion
+      await this.discussionsService.createDiscussion(
+        this.dealId,
+        {
+          topic,
+          clauseHash,
+          clauseIndex,
+          admins: [this.ethereumService.defaultAccountAddress],
+          representatives: [{address: this.ethereumService.defaultAccountAddress}],
+          isPublic: true,
+        },
+      );
+  }
+
 }
--- src/dealDashboard/dealMenubar/dealMenubar.ts
+++ src/dealDashboard/dealMenubar/dealMenubar.ts
@@ -1,6 +1,6 @@
 import { autoinject, bindable } from "aurelia-framework";
 import { DealTokenSwap } from "entities/DealTokenSwap";
-import "./dealNavbar.scss";
+import "./dealMenubar.scss";
 
 @autoinject
 export class DealMenubar {
