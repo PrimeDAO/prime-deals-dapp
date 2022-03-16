1dd9a76126b14f58f8fb964bd53bc23fcd73588f
hiaux0 <hieudn5293@yahoo.de>
2022-03-16 11:48:32 +0100

    refac(dashboard): deal info -> rename class

--- cypress/integration/tests/dealDashboard/deal-description.e2e.ts
+++ cypress/integration/tests/dealDashboard/deal-description.e2e.ts
@@ -5,5 +5,5 @@ Then("I can view the current status of the Partnered Deal", () => {});
 Then("I can view the title of the Partnered Deal", () => {});
 
 And("I can view the description of the Partnered Deal", () => {
-  cy.get("[data-test='dealDescription']").should("be.visible");
+  cy.get("[data-test='dealDescriptionText']").should("be.visible");
 });
