7322f763cb78becd47b90583c135d1ae330f26df
hiaux0 <hieudn5293@yahoo.de>
2022-03-23 11:45:30 +0100

    e2e(discussions): wallet - disconnected (1)

--- cypress/integration/common/deal-dashboard.ts
+++ cypress/integration/common/deal-dashboard.ts
@@ -1,10 +1,24 @@
 import { When } from "@badeball/cypress-cucumber-preprocessor/methods";
-import { PAGE_LOADING_TIMEOUT } from "./test-constants";
+import { E2E_DEALS, PAGE_LOADING_TIMEOUT } from "./test-constants";
+
+export class E2eDealDashboard {
+  public static getContainer() {
+    return cy.get("[data-test='deal-dashboard-container']", { timeout: PAGE_LOADING_TIMEOUT });
+  }
+}
+
+When("I'm viewing a public Deal", () => {
+  const dealId = E2E_DEALS.PUBLIC;
+  const url = `/deal/${dealId}`;
+  cy.visit(url);
+
+  E2eDealDashboard.getContainer().should("be.visible");
+});
 
 When("I'm viewing a private Deal", () => {
   const dealId = "open_deals_stream_hash_1";
   const url = `/deal/${dealId}`;
   cy.visit(url);
 
-  cy.get("[data-test='dealDashboardContainer']", {timeout: PAGE_LOADING_TIMEOUT}).should("be.visible");
+  E2eDealDashboard.getContainer().should("be.visible");
 });
---
+++ cypress/integration/features/dealDashboard/discussions/discussions-wallet.feature
@@ -0,0 +1,20 @@
+Feature: Discussions - Wallet
+  Background:
+    Given I navigate to the Deals home page
+    When I'm viewing a public Deal
+
+  Scenario: Wallet - Disconnected - Deal Clauses
+    Then I cannot begin a Discussion
+
+  Scenario: Wallet - Disconnected - Threads
+    Then I'm informed about who can participate in Discussions
+
+  Scenario: Wallet - Disconnected - Single Comments - Add Comment
+    When I choose a Single Comment without replies
+    Then I cannot add a Comment
+
+  Scenario: Wallet - Disconnected - Single Comments - Comment actions
+    # And I cannot reply to a Comment
+    # And I cannot like a Comment
+    # And I cannot dislike a Comment
+  #   - no vote, comment, reply, (dis)like
