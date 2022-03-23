c0ce9dfd6add48f916a657ea78d2ce31cad97601
Ionel Lupu <ionel.cristian.lupu@gmail.com>
2022-03-23 15:30:51 +0200

    added the funding modal

--- src/dealDashboard/dealVotes/dealVotes.ts
+++ src/dealDashboard/dealVotes/dealVotes.ts
@@ -3,18 +3,34 @@ import { DealTokenSwap } from "entities/DealTokenSwap";
 import "./dealVotes.scss";
 import { Router } from "aurelia-router";
 import { EthereumService } from "../../services/EthereumService";
+import { AlertService } from "../../services/AlertService";
 
 @autoinject
 export class DealVotes {
   @bindable deal: DealTokenSwap;
 
+  fundingModal: HTMLElement;
+
   constructor(
     private router: Router,
     public ethereumService: EthereumService,
+    public alertService: AlertService,
   ) {
   }
 
-  private goto(fragment: string) {
-    this.router.navigate(fragment);
+  async fund() {
+    const result = await this.alertService.showAlert({
+      header: "Start Funding Phase",
+      message: this.fundingModal.innerHTML,
+      buttons: 3,
+      buttonTextPrimary: "Continue",
+      buttonTextSecondary: "Go back",
+    });
+
+    if (result.wasCancelled) {
+      return;
+    }
+
+    this.router.navigate(`fund/${this.deal.id}`);
   }
 }
