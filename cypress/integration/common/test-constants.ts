6b93a60e2fdc4eb568310f88d0f10a6eb0519d92
hiaux0 <hieudn5293@yahoo.de>
2022-03-23 11:45:30 +0100

    e2e(discussions): wallet - disconnected (1)

--- cypress/integration/common/test-constants.ts
+++ cypress/integration/common/test-constants.ts
@@ -1,1 +1,8 @@
 export const PAGE_LOADING_TIMEOUT = 10000;
+
+export const PROPOSAL_LEAD_ADDRESS = "0xE834627cDE2dC8F55Fe4a26741D3e91527A8a498";
+export const FRESHLY_CREATED_ADDRESS = "0xA62fEd560C5E513225423512e0d064Fd9a2B9698";
+
+export const E2E_DEALS = {
+  PUBLIC: "open_deals_stream_hash_2",
+};
--- cypress/integration/tests/dealDashboard/discussion.e2e.ts
+++ cypress/integration/tests/dealDashboard/discussion.e2e.ts
@@ -1,5 +1,60 @@
-import { Then } from "@badeball/cypress-cucumber-preprocessor/methods";
+import { Then, When } from "@badeball/cypress-cucumber-preprocessor/methods";
+import { PAGE_LOADING_TIMEOUT } from "../../common/test-constants";
+
+export class E2eDiscussion {
+  public static getDealClauses() {
+    return cy.get("[data-test='deal-clauses']");
+  }
+
+  public static getAddOrReadButton() {
+    return cy.get("[data-test='add-or-read-button']");
+  }
+
+  public static getDiscussionsList() {
+    return cy.get("[data-test='discussions-list']");
+  }
+
+  public static clickSingleComment({numberOfReplies}: {numberOfReplies: number}) {
+    cy.get("[data-test='single-comment']").within(() => {
+      // Click on first comment, that satisfies match
+      cy.contains("[data-test='number-of-replies']", new RegExp(`^${numberOfReplies}$`))
+        .first()
+        .click();
+    });
+
+    return this;
+  }
+
+  public static waitForNoCommentsText() {
+    cy.get("[data-test='comments-loading']").should("be.visible");
+    cy.get("[data-test='no-comments-text']", {timeout: PAGE_LOADING_TIMEOUT}).should("be.visible");
+  }
+
+  public static getCommentInputSection() {
+    return cy.get("[data-test='comment-input-section']");
+  }
+}
+
+When("I choose a Single Comment without replies", () => {
+  E2eDiscussion
+    .clickSingleComment({numberOfReplies: 0})
+    .waitForNoCommentsText();
+});
 
 Then("I should not be able to see Discussions", () => {
-  cy.get("deal-clauses").should("not.exist");
+  E2eDiscussion.getDealClauses().should("not.exist");
+});
+
+Then("I cannot begin a Discussion", () => {
+  E2eDiscussion.getAddOrReadButton().should("not.exist");
+});
+
+Then("I'm informed about who can participate in Discussions", () => {
+  E2eDiscussion.getDiscussionsList().within(() => {
+    cy.contains("[data-test='discussions-list-instructions'] a", "Connect").should("be.visible");
+  });
+});
+
+Then("I cannot add a Comment", () => {
+  E2eDiscussion.getCommentInputSection().should("not.exist");
 });
