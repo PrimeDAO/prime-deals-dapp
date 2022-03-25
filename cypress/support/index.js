6d6310a05c37ed94562a8d67cddef82ae2480ea3
hdn <hieudn5293@yahoo.de>
2022-03-25 19:22:28 +0100

    chore(cypress): disable logs in gui

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -24,3 +24,16 @@ Cypress.on('uncaught:exception', (err, runnable) => {
   // failing the test
   return false
 })
+
+// Hide fetch/XHR requests
+// https://github.com/cypress-io/cypress/issues/7362#issuecomment-944273204
+const app = window.top;
+
+if (!app.document.head.querySelector('[data-hide-command-log-request]')) {
+  const style = app.document.createElement('style');
+  style.innerHTML =
+    '.command-name-request, .command-name-xhr { display: none }';
+  style.setAttribute('data-hide-command-log-request', '');
+
+  app.document.head.appendChild(style);
+}
