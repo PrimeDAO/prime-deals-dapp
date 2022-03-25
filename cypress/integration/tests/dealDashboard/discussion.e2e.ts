22aeeb9239a9c34055c044d0b2b0702b84e85ebc
hdn <hieudn5293@yahoo.de>
2022-03-25 10:57:23 +0100

    e2e(discussions): single comment - reply

--- cypress/integration/tests/dealDashboard/discussion.e2e.ts
+++ cypress/integration/tests/dealDashboard/discussion.e2e.ts
@@ -2,6 +2,8 @@ import { And, Then, When } from "@badeball/cypress-cucumber-preprocessor/methods
 import { PAGE_LOADING_TIMEOUT } from "../../common/test-constants";
 
 export class E2eDiscussion {
+  public static replyToAddress = "";
+
   public static getDealClauses() {
     return cy.get("[data-test='deal-clauses']");
   }
@@ -14,21 +16,17 @@ export class E2eDiscussion {
     return cy.get("[data-test='discussions-list']");
   }
 
-  public static getSingeComment() {
-    return cy.get("[data-test='single-comment']");
+  public static getSingleTopic() {
+    return cy.get("[data-test='single-topic']");
   }
 
-  public static hoverSingeComment() {
-    return cy.get("[data-test='single-comment']").trigger("pointerover");
-  }
-
-  public static clickSingleComment({numberOfReplies}: {numberOfReplies?: number} = {}) {
+  public static clickSingleTopic({numberOfReplies}: {numberOfReplies?: number} = {}) {
     if (numberOfReplies === undefined) {
-      E2eDiscussion.getSingeComment().first().click();
+      E2eDiscussion.getSingleTopic().first().click();
       return this;
     }
 
-    E2eDiscussion.getSingeComment().within(() => {
+    E2eDiscussion.getSingleTopic().within(() => {
       // Click on first comment, that satisfies match
       cy.contains("[data-test='number-of-replies']", new RegExp(`^${numberOfReplies}$`))
         .first()
@@ -38,28 +36,71 @@ export class E2eDiscussion {
     return this;
   }
 
+  public static getSingleComment() {
+    E2eDiscussion.getSingleCommentAuthor().should("be.visible");
+    return cy.get("[data-test='single-comment']", {timeout: PAGE_LOADING_TIMEOUT}).last();
+  }
+
+  public static getSingleCommentAuthor() {
+    return cy.get("[data-test='single-comment']", {timeout: PAGE_LOADING_TIMEOUT})
+      .last()
+      .find("[data-test='comment-author']", {timeout: PAGE_LOADING_TIMEOUT}); // Comment author info fetched from 3rd party
+  }
+
+  public static hoverSingleComment() {
+    return E2eDiscussion.getSingleComment().within(() => {
+      cy.get("[data-test='single-comment-action']").invoke("show");
+    });
+  }
+
+  public static getReplyActionButton() {
+    return E2eDiscussion.getSingleComment()
+      .find("[data-test='single-comment-action']")
+      .contains("button", "Reply");
+  }
+
+  public static getCancelActionButton() {
+    return E2eDiscussion.getSingleComment()
+      .find("[data-test='single-comment-action']")
+      .contains("button", "Cancel");
+  }
+
   public static waitForNoCommentsText() {
     cy.get("[data-test='comments-loading']").should("be.visible");
     cy.get("[data-test='no-comments-text']", {timeout: PAGE_LOADING_TIMEOUT}).should("be.visible");
   }
 
+  public static getReplyToContainer() {
+    return cy.get("[data-test='reply-to-container']");
+  }
+
   public static getCommentInputSection() {
     return cy.get("[data-test='comment-input-section']");
   }
+
+  public static getCommentInputReply() {
+    return E2eDiscussion
+      .getCommentInputSection()
+      .find("[data-test='comment-input-reply'] button");
+  }
 }
 
-When("I choose a Single Comment without replies", () => {
+When("I choose a single Topic without replies", () => {
   E2eDiscussion
-    .clickSingleComment({numberOfReplies: 0})
+    .clickSingleTopic({numberOfReplies: 0})
     .waitForNoCommentsText();
 });
 
-When("I choose a Single Comment with replies", () => {
+When("I choose a single Topic with replies", () => {
   // TODO: We assume a specific comment setup
   //  Can/should this be generalized?
   //  Eg. the first comment in the specific deal (of this test case), has what we need
   E2eDiscussion
-    .clickSingleComment();
+    .clickSingleTopic();
+});
+
+When("I view a single Comment", () => {
+  E2eDiscussion.hoverSingleComment();
 });
 
 Then("I should not be able to see Discussions", () => {
@@ -81,7 +122,26 @@ Then("I cannot add a Comment", () => {
 });
 
 Then("I can reply to that Comment", () => {
-  E2eDiscussion.hoverSingeComment();
+  // Action on the single comment
+  E2eDiscussion.getReplyActionButton().click();
+  E2eDiscussion.getCancelActionButton().should("be.visible");
+
+  E2eDiscussion.getSingleCommentAuthor().within(() => {
+    cy.get("etherscanlink .text").invoke("text").then(address => {
+      E2eDiscussion.replyToAddress = address.trim();
+    });
+  });
+
+  // Side effects around the comment input section
+  E2eDiscussion.getCommentInputReply().should("be.disabled");
+  E2eDiscussion.getReplyToContainer().should("be.visible");
 });
 
-And("I cannot reply to a Comment", () => {});
+Then("I can see who I am replying to", () => {
+  E2eDiscussion.getReplyToContainer().within(() => {
+    cy.contains("[data-test='reply-to-address']", E2eDiscussion.replyToAddress).should("be.visible");
+  });
+});
+
+And("I cannot reply to a Comment", () => {
+});
