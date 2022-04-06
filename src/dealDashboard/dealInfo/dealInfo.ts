ce5c12009a18f3eb6046a5b25bf33b080e6eb00a
Doug Kent <github@douglaskent.com>
2022-04-06 12:11:55 -0600

    clean up dealInfo

--- src/dealDashboard/dealInfo/dealInfo.ts
+++ src/dealDashboard/dealInfo/dealInfo.ts
@@ -2,30 +2,12 @@ import { autoinject, bindable } from "aurelia-framework";
 import { DealTokenSwap } from "entities/DealTokenSwap";
 import { availableSocialMedias } from "wizards/tokenSwapDealWizard/dealWizardTypes";
 import "./dealInfo.scss";
-import { EthereumService } from "../../services/EthereumService";
 
 @autoinject
 export class DealInfo {
   @bindable deal: DealTokenSwap;
 
-  timeLeftToExecute?: number;
-  timeLeftToExecuteInterval?: number;
-
-  constructor(public ethereumService: EthereumService) {
-  }
-
-  attached() {
-    this.timeLeftToExecute = this.deal.timeLeftToExecute;
-    this.timeLeftToExecuteInterval = window.setInterval(() => {
-      this.timeLeftToExecute = this.deal.timeLeftToExecute;
-    }, 1000);
-  }
-
   getSocialMediaDetails(socialMediaName: string) {
     return availableSocialMedias.find(socialMedia => socialMedia.name === socialMediaName);
-  }
-
-  detached() {
-    clearInterval(this.timeLeftToExecuteInterval);
   }
 }
