6839fc3e79a488743dd3462086a39dad028c7fad
hdn <hieudn5293@yahoo.de>
2022-04-06 17:49:25 +0200

    e2e(deals): specify deal you want to create

--- cypress/integration/tests/deals/open-proposal.2e2.ts
+++ cypress/integration/tests/deals/open-proposal.2e2.ts
@@ -1,34 +1,34 @@
 import { Given, Then } from "@badeball/cypress-cucumber-preprocessor/methods";
-import { MINIMUM_OPEN_PROPOSAL } from "../../../fixtures/dealFixtures";
 import { E2eDealWizard } from "../../common/pageObjects/dealWizard";
 import { EditingCard } from "../components/editingCard.e2e";
 import { E2eWallet } from "../wallet.e2e";
+import { E2eDeals } from "./deals.e2e";
 import { Terms } from "./terms.e2e";
 
 Given("I fill out the Proposal Stage", () => {
   E2eDealWizard.inWizardSection("Proposal")
-    .inField("Proposal Title").fillIn(MINIMUM_OPEN_PROPOSAL.proposal.title)
-    .inField("Proposal Summary").fillIn(MINIMUM_OPEN_PROPOSAL.proposal.summary)
-    .inField("Proposal Description").fillIn(MINIMUM_OPEN_PROPOSAL.proposal.description);
+    .inField("Proposal Title").fillIn(E2eDeals.currentDeal.proposal.title)
+    .inField("Proposal Summary").fillIn(E2eDeals.currentDeal.proposal.summary)
+    .inField("Proposal Description").fillIn(E2eDeals.currentDeal.proposal.description);
 });
 
 Given("I fill out the Lead Details Stage", () => {
-  const address = MINIMUM_OPEN_PROPOSAL.proposalLead.address || E2eWallet.currentWalletAddress;
+  const address = E2eDeals.currentDeal.proposalLead.address || E2eWallet.currentWalletAddress;
   E2eDealWizard.inWizardSection("Proposal")
     .inField("Proposal Lead Address").fillIn(address);
 });
 
 Given("I fill out the Primary DAO Stage", () => {
   E2eDealWizard.inWizardSection("Primary DAO")
-    .inField("DAO Name").fillIn(MINIMUM_OPEN_PROPOSAL.primaryDAO.name)
-    .inField("DAO Treasury Address").fillIn(MINIMUM_OPEN_PROPOSAL.primaryDAO.treasury_address)
-    .inField("DAO Avatar").fillIn(MINIMUM_OPEN_PROPOSAL.primaryDAO.logoURI);
+    .inField("DAO Name").fillIn(E2eDeals.currentDeal.primaryDAO.name)
+    .inField("DAO Treasury Address").fillIn(E2eDeals.currentDeal.primaryDAO.treasury_address)
+    .inField("DAO Avatar").fillIn(E2eDeals.currentDeal.primaryDAO.logoURI);
 
   E2eDealWizard.inWizardSection("Select Representatives")
-    .inField("DAO Representatives Addresses").fillIn(MINIMUM_OPEN_PROPOSAL.primaryDAO.representatives[0].address);
+    .inField("DAO Representatives Addresses").fillIn(E2eDeals.currentDeal.primaryDAO.representatives[0].address);
 
-  if (MINIMUM_OPEN_PROPOSAL.primaryDAO.representatives.length > 1) {
-    MINIMUM_OPEN_PROPOSAL.primaryDAO.representatives.forEach((representative, index) => {
+  if (E2eDeals.currentDeal.primaryDAO.representatives.length > 1) {
+    E2eDeals.currentDeal.primaryDAO.representatives.forEach((representative, index) => {
       /** Already addded first representative */
       if (index === 0) return;
 
@@ -37,16 +37,15 @@ Given("I fill out the Primary DAO Stage", () => {
         .inField("DAO Representatives Addresses").fillIn(representative.address);
     });
   }
-
 });
 
 Given("I fill out the Terms Stage", () => {
   E2eDealWizard.inWizardSection("Deal Clauses");
-  Terms.getClausesTextarea().type(MINIMUM_OPEN_PROPOSAL.terms.clauses[0].text);
+  Terms.getClausesTextarea().type(E2eDeals.currentDeal.terms.clauses[0].text);
   EditingCard.click("Save");
 
-  if (MINIMUM_OPEN_PROPOSAL.terms.clauses.length > 1) {
-    MINIMUM_OPEN_PROPOSAL.terms.clauses.forEach((clause, index) => {
+  if (E2eDeals.currentDeal.terms.clauses.length > 1) {
+    E2eDeals.currentDeal.terms.clauses.forEach((clause, index) => {
       /** Already addded first clause */
       if (index === 0) return;
 
@@ -57,7 +56,7 @@ Given("I fill out the Terms Stage", () => {
     });
   }
 
-  Terms.getClausesText().should("have.length", MINIMUM_OPEN_PROPOSAL.terms.clauses.length);
+  Terms.getClausesText().should("have.length", E2eDeals.currentDeal.terms.clauses.length);
 });
 
 Then("I can see stages correct for open proposal", () => {
