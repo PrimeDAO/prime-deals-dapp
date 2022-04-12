823fd4da44f783190d10867594674eb2cea6010e
hdn <hieudn5293@yahoo.de>
2022-04-13 01:46:07 +0200

    feat(valueConverter): add dateDiff

---
+++ src/resources/value-converters/dateDiff.ts
@@ -0,0 +1,12 @@
+﻿import { autoinject } from "aurelia-framework";
+import { DateService } from "../../services/DateService";
+
+@autoinject
+export class DateDiffValueConverter {
+  constructor(private dateService: DateService) { }
+
+  public toView(dateString: string): string | null {
+    const result = this.dateService.formattedTime(new Date(dateString)).diff();
+    return result;
+  }
+}
