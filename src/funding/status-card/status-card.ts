7b0c29418c1e261d9b1c198da97b262e65cfd84e
Chad Cartwright <vipergtsrz@gmail.com>
2022-04-18 18:08:02 -0500

    Clean up the status card component

--- src/funding/status-card/status-card.ts
+++ src/funding/status-card/status-card.ts
@@ -9,10 +9,9 @@ export class StatusCard {
   @bindable deal: DealTokenSwap;
   @bindable isPrimary: boolean;
 
-  @computedFrom("swapCompleted", "deal.isFailed", "tokenDao.tokens")
+  @deepComputedFrom("deal.isFailed", "deal.isClaiming", "deal.isFullyClaimed", "deal.isExecuted", "tokenDao.tokens")
   get chipColor(): string{
-    const test = this.isDaoFullyClaimed;
-    if (this.swapCompleted || test){
+    if ((this.deal.isClaiming && this.deal.isFullyClaimed) || this.isDaoFullyClaimed()){
       return "success";
     }
     if (this.tokenDao.tokens.every((x: ITokenCalculated) => x.fundingRequired?.lte(0))){
@@ -22,51 +21,39 @@ export class StatusCard {
     }
   }
 
-  @computedFrom("swapCompleted", "deal.isFailed", "deal.isClaiming", "tokenDao.tokens")
+  @deepComputedFrom("deal.isFailed", "deal.isClaiming", "deal.isFullyClaimed", "deal.isExecuted", "tokenDao.tokens")
   get status(): string{
-    if (this.swapCompleted){
+    if (this.deal.isClaiming && this.deal.isFullyClaimed){
       return "Swap completed";
     } else if (this.deal.isClaiming){
-      if (this.isDaoFullyClaimed){
+      if (this.isDaoFullyClaimed()){
         return "Claiming Completed";
       }
       return "Claiming in progress";
     }
-    if (this.isDaoTargetReached){
+    if ((this.tokenDao.tokens as ITokenCalculated[]).every(x => BigNumber.from(x.amount).eq(x.fundingDeposited ?? 0))){
       return "Target reached";
     } else {
       return this.deal.isFailed ? "Target not reached" : "Funding in progress";
     }
   }
 
-  @computedFrom("deal.isClaiming", "deal.isFullyClaimed")
-  private get swapCompleted() : boolean {
-    return this.deal.isClaiming && this.deal.isFullyClaimed;
-  }
-
-  @computedFrom("tokenDao.tokens")
-  private get isDaoFullyClaimed() : boolean {
+  private isDaoFullyClaimed() : boolean {
     //return true if all tokens for this dao have been claimed
     if (!this.deal.isExecuted) return false;
     return (this.tokenDao.tokens as ITokenCalculated[]).every(x => BigNumber.from(x.amount).eq(x.claimingClaimed?.add(x.instantTransferAmount) ?? 0));
-  }
-
-  @computedFrom("tokenDao.tokens")
-  private get isDaoTargetReached() : boolean {
-    //return true if all tokens for this dao have reached their funding target
-    return (this.tokenDao.tokens as ITokenCalculated[]).every(x => BigNumber.from(x.amount).eq(x.fundingDeposited ?? 0));
   }
 
   private getTotalClaimed(claimingClaimed: ITokenCalculated["claimingClaimed"], instantTransferAmount: ITokenCalculated["instantTransferAmount"]): BigNumber{
     return claimingClaimed?.add(BigNumber.from(instantTransferAmount));
   }
 
-  @computedFrom("deal", "isPrimary")
+  @computedFrom("isPrimary")
   private get firstDao(): IDAO{
     return this.isPrimary ? this.deal.primaryDao : this.deal.partnerDao;
   }
 
-  @computedFrom("deal", "isPrimary")
+  @computedFrom("isPrimary", "deal.isExecuted")
   private get tokenDao(): IDAO {
     if (this.deal.isExecuted){
       if (this.isPrimary){
