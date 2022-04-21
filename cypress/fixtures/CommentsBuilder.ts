3b59abad6e2bd2b2fd4ebf34d7836e171230dd49
hdn <hieudn5293@yahoo.de>
2022-04-21 20:33:18 +0200

    e2e(fixtures): add privacy

--- cypress/fixtures/CommentsBuilder.ts
+++ cypress/fixtures/CommentsBuilder.ts
@@ -15,11 +15,6 @@ export class CommentBuilder {
     // editHistory: [],
     metadata: {
       isPrivate: "false",
-      allowedMembers: `[
-        "0xE834627cDE2dC8F55Fe4a26741D3e91527A8a498",
-        "0xC6228945005F07254b816F82fDA37f6Cd401831d",
-        "0xc8f056AAd3e320809047Af389D658E21412325Aa",
-      ]`,
       encrypted: "",
       iv: "",
     },
@@ -44,6 +39,11 @@ export class CommentBuilder {
 
   public withText(text: string): CommentBuilder {
     this.comment.text = text;
+    return this;
+  }
+
+  public withPrivacy(isPrivate: "true" | "false"): CommentBuilder {
+    this.comment.metadata.isPrivate = isPrivate;
     return this;
   }
 
