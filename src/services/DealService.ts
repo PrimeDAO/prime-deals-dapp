eaaf3e18f09647b5e36cc3341d9be726818f47f2
Pawel Janicki <paweljanicki256@gmail.com>
2022-04-23 02:27:58 +0800

    Improve performance of getDealInfo by awaiting all promises in parallel (#743)
    * Improve performance of getDealInfo by awaiting all promises in parallel
    
    * cleaner and faster?
    
    Co-authored-by: Doug Kent <github@douglaskent.com>

--- src/services/DealService.ts
+++ src/services/DealService.ts
@@ -1,7 +1,7 @@
 import { skip } from "rxjs/operators";
 import { SortOrder, SortService } from "services/SortService";
 import { IDealRegistrationTokenSwap } from "entities/DealRegistrationTokenSwap";
-import { Address, EthereumService, Networks } from "./EthereumService";
+import { Address, EthereumService, IBlockInfoNative, Networks } from "./EthereumService";
 import { autoinject, computedFrom, Container, TaskQueue } from "aurelia-framework";
 import { DealTokenSwap } from "entities/DealTokenSwap";
 import { EventAggregator } from "aurelia-event-aggregator";
@@ -193,17 +193,21 @@ export class DealService {
     const moduleContract = await this.contractsService.getContractFor(ContractNames.TOKENSWAPMODULE);
     const filter = moduleContract.filters.TokenSwapExecuted();
     const dealIds = new Map<string, IExecutedDeal>();
+    const promises = new Array<Promise<void>>();
 
     await moduleContract.queryFilter(filter, StartingBlockNumber)
       .then(async (events: Array<IStandardEvent<ITokenSwapExecutedArgs>>): Promise<void> => {
         for (const event of events) {
           const params = event.args;
-          // hack until the event has it
-          // const dealId = (await moduleContract.tokenSwaps(params.dealId)).metadata;
           const dealId = parseBytes32String(params.metadata);
-          dealIds.set(dealId, { executedAt: new Date((await event.getBlock()).timestamp * 1000) });
-        }
-      });
+          promises.push(event.getBlock()
+            .then((block: IBlockInfoNative) => {
+              dealIds.set(dealId, { executedAt: new Date(block.timestamp * 1000) });
+            }));
+        }});
+
+    // no need to await
+    Promise.all(promises);
 
     // TODO figure out how to gkeep this up-to-date
     this.executedDealIds = dealIds;
@@ -214,15 +218,21 @@ export class DealService {
     const moduleContract = await this.contractsService.getContractFor(ContractNames.TOKENSWAPMODULE);
     const filter = moduleContract.filters.TokenSwapCreated();
     const dealIds = new Map<string, IFundedDeal>();
+    const promises = new Array<Promise<void>>();
 
     await moduleContract.queryFilter(filter, StartingBlockNumber)
       .then(async (events: Array<IStandardEvent<ITokenSwapCreatedArgs>>): Promise<void> => {
         for (const event of events) {
           const params = event.args;
           const dealId = parseBytes32String(params.metadata);
-          dealIds.set(dealId, { fundedAt: new Date((await event.getBlock()).timestamp * 1000) });
-        }
-      });
+          promises.push(event.getBlock()
+            .then((block: IBlockInfoNative) => {
+              dealIds.set(dealId, { fundedAt: new Date(block.timestamp * 1000) });
+            }));
+        }});
+
+    // no need to await
+    Promise.all(promises);
 
     // TODO figure out how to gkeep this up-to-date
     this.fundedDealIds = dealIds;
