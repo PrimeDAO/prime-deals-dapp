bbf4ec65c336d4f4e7eae1f20b8cff69aeb550da
hdn <hieudn5293@yahoo.de>
2022-04-28 21:09:11 +0200

    feat(test.data): update test data

--- test/data/index.ts
+++ test/data/index.ts
@@ -3,6 +3,7 @@ import * as json2 from "./floresta-x-climati-partnede-deal.json";
 import * as json3 from "./minimal-open-proposal.json";
 import * as json4 from "./minimal-partnered-deal.json";
 import * as json5 from "./private-partnered-deal.json";
+import * as fundingDeals from "./dealFixturesFunding.json";
 
-export const jsonDocs = [json1, json2, json3, json4, json5];
+export const jsonDocs = [json1, json2, json3, json4, json5, ...Array.from(fundingDeals)];
 export default jsonDocs;
