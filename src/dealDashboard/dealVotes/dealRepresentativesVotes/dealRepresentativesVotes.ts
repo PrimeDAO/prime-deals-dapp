68e4bcd2a7ca9e6c57199a9689b546f4ea28fd9c
vipergtsrz <vipergtsrz@gmail.com>
2022-05-04 11:58:56 -0500

    Voting: Red Circle goes to left and right while interacting with Voting buttons (#882)

--- src/dealDashboard/dealVotes/dealRepresentativesVotes/dealRepresentativesVotes.ts
+++ src/dealDashboard/dealVotes/dealRepresentativesVotes/dealRepresentativesVotes.ts
@@ -1,14 +1,12 @@
 import { autoinject, bindable, computedFrom } from "aurelia-framework";
-import { IDAO, IRepresentative } from "../../../entities/DealRegistrationTokenSwap";
+import { IDAO } from "../../../entities/DealRegistrationTokenSwap";
 import "./dealRepresentativesVotes.scss";
-import { DealTokenSwap } from "../../../entities/DealTokenSwap";
 import { IVotesInfo } from "../../../entities/IDealTypes";
 
 @autoinject
 export class DealRepresentativesVotes {
-  @bindable deal: DealTokenSwap;
+  @bindable votes: IVotesInfo;
   @bindable dao: IDAO;
-
   showMore = false;
 
   panel: HTMLElement;
@@ -18,21 +16,13 @@ export class DealRepresentativesVotes {
     return this.showMore ? this.panel.scrollHeight + "px" : "";
   }
 
-  getVoteSlug(allVotes: IVotesInfo, representativeAddress: string) {
-    return allVotes[representativeAddress] === true
-      ? "accepted"
-      : allVotes[representativeAddress] === false
-        ? "declined"
-        : "";
-  }
-
   shouldShowDelimited(count: number, index: number) {
     return this.dao.representatives.length > 2 && !Math.round(index - count / 2);
   }
 
-  representativesSortedByVotes(representatives: IRepresentative[], allVotes: IVotesInfo) {
-    return this.dao.representatives.sort((a, b) => {
-      return allVotes[a.address] > allVotes[b.address] ? -1 : 1;
-    });
+  @computedFrom("votes")
+  private get sortedVotes() : boolean[] {
+    return this.dao.representatives.map(x => this.votes[x.address]).sort((a, b) => a > b ? -1 : 1);
   }
+
 }
