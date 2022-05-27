b20b7470ac18e6b3fa514ebe185a2433d1db41b4
Brandon Seydel <brandonseydel@hotmail.com>
2022-05-27 17:18:37 -0500

    fix home page

--- src/home/home.ts
+++ src/home/home.ts
@@ -1,48 +1,47 @@
-import { singleton } from "aurelia";
+import { Router } from "aurelia";
+import { DealTokenSwap } from "entities/DealTokenSwap";
+import { DealService } from "services/DealService";
 
-// type DealType = "open" | "partnered";
-@singleton({scoped: false}) // to maintain tab selection state TODO test with and without argument
+ type DealType = "open" | "partnered";
 export class Home {
-  // private cardIndex = 0;
-  // private dealType:DealType = "open";
-  // private dealsLoading = false;
-  // static MAX_DEALS_COUNT=10;
-  //
-  // constructor(
-  //   private router: Router,
-  //   private dealService: DealService,
-  // ) {
-  // }
-  //
-  // @computedFrom("dealService.openProposals", "dealService.partneredDeals")
-  // private get allDeals(): Record<"open" | "partnered", Array<DealTokenSwap>> {
-  //   return {
-  //     open: this.dealService.openProposals.slice(0, Home.MAX_DEALS_COUNT),
-  //     partnered: this.dealService.partneredDeals.slice(0, Home.MAX_DEALS_COUNT),
-  //   };
-  // }
-  //
-  // @computedFrom("allDeals", "dealType")
-  // private get featuredDeals(): Array<DealTokenSwap> {
-  //   return this.allDeals[this.dealType].slice(0, 10).filter((deal: DealTokenSwap) => !deal.isCancelled && !deal.isFailed);
-  // }
-  //
-  // async attached(): Promise<void> {
-  //   this.dealsLoading = true;
-  //   await this.dealService.ensureAllDealsInitialized();
-  //   this.dealsLoading = false;
-  //
-  //   if (this.cardIndex === undefined) {
-  //     this.cardIndex = this.allDeals.open.length ? 0 : 1;
-  //   }
-  // }
-  //
-  // dealToggle(index: number, type: "open" | "partnered"): void {
-  //   this.dealType = type;
-  //   this.cardIndex = index;
-  // }
-  //
-  // navigate(href: string): void {
-  //   this.router.navigate(href);
-  // }
+  private cardIndex = 0;
+  private dealType: DealType = "open";
+  private dealsLoading = false;
+  static MAX_DEALS_COUNT = 10;
+
+  constructor(
+    private router: Router,
+    private dealService: DealService,
+  ) {
+  }
+
+  private get allDeals(): Record<"open" | "partnered", Array<DealTokenSwap>> {
+    return {
+      open: this.dealService.openProposals.slice(0, Home.MAX_DEALS_COUNT),
+      partnered: this.dealService.partneredDeals.slice(0, Home.MAX_DEALS_COUNT),
+    };
+  }
+
+  private get featuredDeals(): Array<DealTokenSwap> {
+    return this.allDeals[this.dealType].slice(0, 10).filter((deal: DealTokenSwap) => !deal.isCancelled && !deal.isFailed);
+  }
+
+  async attached(): Promise<void> {
+    this.dealsLoading = true;
+    await this.dealService.ensureAllDealsInitialized();
+    this.dealsLoading = false;
+
+    if (this.cardIndex === undefined) {
+      this.cardIndex = this.allDeals.open.length ? 0 : 1;
+    }
+  }
+
+  dealToggle(index: number, type: "open" | "partnered"): void {
+    this.dealType = type;
+    this.cardIndex = index;
+  }
+
+  navigate(href: string): void {
+    this.router.load(href);
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { StandardConfiguration } from '@aurelia/runtime-html';
-import Aurelia, { DialogDefaultConfiguration, RouterConfiguration, } from 'aurelia';
+import Aurelia, { DialogDefaultConfiguration, Registration, RouterConfiguration, } from 'aurelia';
+import { IDataSourceDeals } from 'services/DataSourceDealsTypes';
+import { FirestoreDealsService } from 'services/FirestoreDealsService';
 import { App } from './app';
 import * as ResourcesComponents from './resources/index';
 import { register as services } from './services/register';
@@ -12,5 +14,6 @@ new Aurelia()
   .register(ResourcesComponents)
   .register(DialogDefaultConfiguration)
   .register(services)
+  .register(Registration.singleton(IDataSourceDeals, FirestoreDealsService))
   .app(App)
   .start()
