bb64468c74057d701254a6baa01631ff2c886d2e
Brandon Seydel <brandonseydel@hotmail.com>
2022-06-09 06:41:50 -0500

    fix wrong selector

--- src/resources/elements/primeDesignSystem/pinput-group/pinput-group.ts
+++ src/resources/elements/primeDesignSystem/pinput-group/pinput-group.ts
@@ -4,8 +4,8 @@ import { bindable, children, customElement } from "aurelia";
 export class PInputGroup {
   element: Element;
   @bindable disabled = false;
-  @children({ query: x => x.host.querySelectorAll("[slot='before']") }) beforeSlots;
-  @children({ query: x => x.host.querySelectorAll("[slot='after']")}) afterSlots;
+  @children({ query: x => x.host.querySelectorAll("[au-slot='before']") }) beforeSlots;
+  @children({ query: x => x.host.querySelectorAll("[au-slot='after']")}) afterSlots;
 
   constructor(element: Element) {
     this.element = element;
