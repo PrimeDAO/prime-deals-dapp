7f0170908c2d11817253d0272c7e1889bdb0968c
Chad Cartwright <vipergtsrz@gmail.com>
2022-06-14 16:42:48 -0500

    Fix the maxlength to not render when not passed

--- src/resources/elements/primeDesignSystem/pinput-text/pinput-text.ts
+++ src/resources/elements/primeDesignSystem/pinput-text/pinput-text.ts
@@ -3,10 +3,17 @@ import { bindable, BindingMode, customElement } from "aurelia";
 
 @customElement("pinput-text")
 export class PInputText {
-
+  private input: HTMLInputElement;
+  @bindable max: number;
   @bindable validationState?: ValidationState;
   @bindable autocomplete = "off";
   @bindable disabled = false;
   @bindable({mode: BindingMode.twoWay}) value: string;
   @bindable placeholder = "";
+
+  bound(){
+    if (this.input && this.max > 0){
+      this.input?.setAttribute("maxlength", `${this.max}`);
+    }
+  }
 }
--- src/resources/elements/primeDesignSystem/ptextarea/ptextarea.ts
+++ src/resources/elements/primeDesignSystem/ptextarea/ptextarea.ts
@@ -5,13 +5,20 @@ import { bindable, BindingMode, customElement } from "aurelia";
 
 @customElement("ptextarea")
 export class PTextarea {
+  private textarea: HTMLTextAreaElement;
 
   @bindable validationState?: ValidationState;
-  @bindable max?: number;
+  @bindable max: number;
   @bindable autocomplete = "off";
   @bindable disabled = false;
   @bindable rows = 4;
   @bindable({mode: BindingMode.twoWay}) value: string | BigNumber;
   @bindable placeholder = "";
   @bindable resizable = true;
+
+  bound(){
+    if (this.textarea && this.max > 0){
+      this.textarea?.setAttribute("maxlength", `${this.max}`);
+    }
+  }
 }
