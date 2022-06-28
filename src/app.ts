6f819525a2b4b9ec46c3101a7c1f972c96dd7338
hdn <hieudn5293@yahoo.de>
2022-06-28 21:31:06 +0200

    fix(app): text copy "launchs" -> "deals"

--- src/app.ts
+++ src/app.ts
@@ -86,7 +86,7 @@ export class App {
 
       let notChanged = true;
       const connect = await this.alertService.showAlert( {
-        message: `You are connecting to ${info.connectedTo ?? "an unknown network"}, but to interact with launches we need you to connect to ${info.need}.  Do you want to switch your connection ${info.need} now?`,
+        message: `You are connecting to ${info.connectedTo ?? "an unknown network"}, but to interact with deals we need you to connect to ${info.need}.  Do you want to switch your connection ${info.need} now?`,
         // eslint-disable-next-line no-bitwise
         buttons: ShowButtonsEnum.Primary | ShowButtonsEnum.Secondary,
         buttonTextPrimary: "Yes, Please",
