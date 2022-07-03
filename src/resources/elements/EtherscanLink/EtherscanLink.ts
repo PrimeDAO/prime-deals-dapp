c4e6faca72d06484d2b1cf64e7b8b09eecdd83b5
Ionel Lupu <ionel.cristian.lupu@gmail.com>
2022-07-03 21:28:18 +0300

    showing the ens when it's available

--- src/resources/elements/EtherscanLink/EtherscanLink.ts
+++ src/resources/elements/EtherscanLink/EtherscanLink.ts
@@ -1,24 +1,26 @@
-﻿import { customElement, bindable, BindingMode } from "aurelia";
+﻿import { bindable, BindingMode, customElement } from "aurelia";
 import { toBoolean } from "resources/binding-behaviours";
 import { IEthereumService } from "services/EthereumService";
+import { EnsService, Utils } from "../../../services";
 
 @customElement("etherscanlink")
 export class EtherscanLink {
-  @bindable({ mode: BindingMode.oneTime }) public address: string;
-  @bindable({ mode: BindingMode.oneTime }) public text?: string;
-  @bindable({ mode: BindingMode.oneTime }) public type: string;
+  @bindable({mode: BindingMode.oneTime}) public address: string;
+  @bindable({mode: BindingMode.oneTime}) public text?: string;
+  @bindable({mode: BindingMode.oneTime}) public type: string;
   /**
    * set add classes on the text
    */
-  @bindable({ mode: BindingMode.oneTime }) public css: string;
-  @bindable({ set: toBoolean, type: Boolean, mode: BindingMode.oneTime }) public hideClipboardButton: boolean;
+  @bindable({mode: BindingMode.oneTime}) public css: string;
+  @bindable({set: toBoolean, type: Boolean, mode: BindingMode.oneTime}) public hideClipboardButton: boolean;
   /**
    * bootstrap config for a tooltip
    */
-  // @bindable({ defaultBindingMode: bindingMode.oneTime }) public tooltip?: any;
+    // @bindable({ defaultBindingMode: bindingMode.oneTime }) public tooltip?: any;
 
   private copyMessage: string;
   private internal = false;
+  ens?: string;
 
   private get networkExplorerUri(): string {
     return this.ethereumService.getEtherscanLink(this.address, this.type === "tx");
@@ -26,6 +28,7 @@ export class EtherscanLink {
 
   constructor(
     @IEthereumService private ethereumService: IEthereumService,
+    private ensService: EnsService,
   ) { }
 
   public attached(): void {
@@ -34,5 +37,24 @@ export class EtherscanLink {
     } else {
       this.copyMessage = "Address has been copied to the clipboard";
     }
+  }
+
+  async binding() {
+    await this.addressChanged(this.address);
+  }
+
+  async addressChanged(newValue: string) {
+
+    if (Utils.isAddress(newValue)) {
+      this.ens = (await this.ensService.getEnsForAddress(newValue)) ?? "";
+    } else {
+      const address = await this.ensService.getAddressForEns(newValue);
+      if (address) {
+        this.ens = newValue;
+      } else {
+        this.ens = "";
+      }
+    }
+
   }
 }
